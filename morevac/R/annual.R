c96ab92f32040fa35a1cc75100973d6bb1914c56
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-05-07 13:09:55 +0100

    Upload MoreVac package

---
+++ morevac/R/annual.R
@@ -0,0 +1,127 @@
+### annual vaccination ###
+# scenarios:
+# 1. random children vaccinated each year
+# 2. same children vaccinated each year
+#setwd("~/Google Drive/morevac")
+setwd("C:/Users/kainslie/Google Drive/morevac")
+source("R/morevac_functions.R")
+
+library(ggplot2)
+### Scenario 1: random annual vaccination
+run_annual1 <- function(n=100, years=5,vac_threshold=3,vac_coverage=0.5,
+                        mybeta=0.15,delta_x=0.2,delta_v=0.3,mygamma=0.4,biannual=FALSE,
+                        same_indiv = FALSE){
+  init <- initialize_pop(nindiv=n,timepoints=years)
+  suscept_mat <- init$susceptibility
+  vac_hist_mat <- init$vac_history
+  inf_hist_mat <- init$infection_matrix
+  x <- init$x; v <- x
+  attack_rate <- init$ar; vac_ar <- attack_rate; unvac_ar <- attack_rate
+  incidence <- init$incidence
+  
+  if (same_indiv){
+    vn <- sample(1:n,n*vac_coverage)
+  }
+  
+  # start loop over years
+  for (year in 1:years){
+     for(i in 1:n){
+      # generate random number for infection
+      if (same_indiv){
+        if (i %in% vn) {
+          randnum_vac <- vac_coverage
+        } else randnum_vac <- 1
+        
+      } else {
+        randnum_vac <- runif(1,0,1)
+      }
+      # vaccinate
+        if (randnum_vac <= vac_coverage & year >= vac_threshold){
+          if (biannual == FALSE) {
+            vac_hist_mat[i,year] <- 1
+            v[i,year] <- 0
+          } else if (biannual == TRUE & (year %% 2) != 0){
+            vac_hist_mat[i,year] <- 1
+            v[i,year] <- 0
+          } else {vac_hist_mat[i,year] <- 0}
+        } else {vac_hist_mat[i,year] <- 0}
+
+        #print(vac_hist_mat[i,])
+      # generate random number for infection
+        randnum_inf <- runif(1,0,1)
+      # calculate susceptibility function for person i
+        #print(x[i,])
+        #print(vac_hist_mat[i,])
+        
+        # never infected
+        if (x[i,year] == 999){
+          if (v[i,year] == 999 | v[i,year] > 1/delta_v){ # never vaccinated or vaccinated long enough ago for drift to have diminished protection
+            suscept_mat[i,year] <- 1
+          } else if (v[i,year]==0 | v[i,year]<=1/delta_v){ # vaccinated this year or within last few years 
+            suscept_mat[i,year] <- (vac_hist_mat[i,year]*mygamma) + (v[i,year]*delta_v)
+          } 
+        }
+        
+        # infected and delta>0
+        if (x[i,year]>=0 & x[i,year]<999 & delta_x>0 & delta_v>0){
+          if (v[i,year]==999 | v[i,year] > 1/delta_v){ # never vaccinated
+            if (x[i,year]< 1/delta_x){
+              suscept_mat[i,year] <- x[i,year]*delta_x
+            } else if (x[i,year]>= 1/delta_x) {
+              suscept_mat[i,year] <- 1
+            } 
+          } else if (v[i,year]==0 | v[i,year]<=1/delta_v){ # vaccinated
+            suscept_mat[i,year] <- min(x[i,year]*delta_x,(vac_hist_mat[i,year]*mygamma)+(v[i,year]*delta_v))
+          } 
+        }
+        
+        # infected and delta=0
+        if (x[i,year]>=0 & x[i,year]<999 & delta_x==0 & delta_v==0){
+          suscept_mat[i,year] <- 0
+        }
+        
+      # infect person i if random number < beta*susceptability
+        #print(suscept_mat[i,])
+        if (randnum_inf<=mybeta*suscept_mat[i,year]) {
+          inf_hist_mat[i,year] <- 1
+          x[i,year] <- 0
+        } else {inf_hist_mat[i,year] <- 0}
+      # update x for the next year
+        if (year<years){
+          x[i,year+1] <- ifelse(x[i,year]<999,x[i,year]+1,x[i,year])
+          v[i,year+1] <- ifelse(v[i,year]<999,v[i,year]+1,v[i,year])
+        }
+    }
+    
+    # calculate attack rate
+    attack_rate[year] <- sum(inf_hist_mat[,year])/n
+    vac <- which(vac_hist_mat[,year]==1)
+    unvac <- which(vac_hist_mat[,year]==0)
+    vac_ar[year] <- sum(inf_hist_mat[vac,year])/n
+    unvac_ar[year] <- sum(inf_hist_mat[unvac,year])/n
+    incidence[year] <- sum(inf_hist_mat[,year])
+  }
+  rtn <- list(infection_matrix=inf_hist_mat,x=x,vac_history=vac_hist_mat,
+              susceptibility=suscept_mat,attack_rate=attack_rate,incidence=incidence)
+  dat <- data.frame(Year=1:years,Attack_Rate=attack_rate,Incidence=incidence)
+  dat2 <- data.frame(Year=rep(1:years,2),Attack_Rate=c(vac_ar,unvac_ar),Vac_Status=c(rep("Vaccinated",years),rep("Unvaccinated",years)))
+  dat2 <-dat2[order(dat2$Year),]
+  p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate)) +       
+        geom_line()
+  
+  p2 <- ggplot(data = dat2, aes(x = Year, y = Attack_Rate, colour= Vac_Status)) +       
+        geom_line() +
+        geom_vline(xintercept=2, linetype="dashed", color = "black") +
+        theme(panel.grid.major = element_blank(), 
+              panel.grid.minor = element_blank(),
+              panel.background = element_blank(), 
+              axis.line = element_line(colour = "black"),
+              legend.position = c(.95, .95),
+              legend.justification = c("right", "top"),
+              legend.box.just = "right",
+              legend.margin = margin(6, 6, 6, 6),
+              legend.key = element_rect(fill = "white")
+        )
+  
+  return(list(rtn,p1,p2))
+}
---
+++ morevac/R/morevac_functions.R
@@ -0,0 +1,142 @@
+### MoReVac - Modelling Repeat Vaccination ###
+### Agent-based model of repeat vaccination in birth cohort
+
+    
+#' Initialize population
+#'
+#' This function initializes the population before running the model.
+#' @param nindiv Number of individuals in the simulated population. Defaults to 1000.
+#' @param timepoints Number of timepoints to simulate over. Defaults to 10.
+#' @return List containing initialized infection history matrix, vaccination history matrix, susceptibility matrix, attack rate vector, and incidence vector.
+#' @keywords morevac
+#' @export
+#' @examples
+#' initialize_pop()    
+initialize_pop <- function(nindiv=1000,timepoints=10){
+  # infection status matrix
+    inf_stat_mat <- matrix(c(rep(NA, timepoints*nindiv)),nrow=nindiv)
+    colnames(inf_stat_mat) <- c(paste0("AY",0:(timepoints-1)))
+  # years since last infection matrix  
+    x <- matrix(c(rep(999, timepoints*nindiv)),nrow=nindiv)
+    colnames(x) <- c(paste0("AY",0:(timepoints-1)))
+    
+  # vaccination history matrix
+    vac_hist_mat <- matrix(c(rep(NA, timepoints*nindiv)),nrow=nindiv)
+    colnames(vac_hist_mat) <- c(paste0("AY",0:(timepoints-1)))
+  # susceptibility matrix
+    suscept_mat <- matrix(c(rep(1,timepoints*nindiv)),nrow=nindiv)
+    colnames(suscept_mat) <- c(paste0("AY",0:(timepoints-1)))
+  # ages
+  # ages <- c(rep(0,nindiv))
+    attack_rate <- c(rep(NA,timepoints))
+    incidence <- c(rep(NA,timepoints))
+    
+    return(list(infection_matrix=inf_stat_mat,x=x,vac_history=vac_hist_mat,
+                susceptibility=suscept_mat,attack_rate=attack_rate,incidence=incidence))
+}
+### susceptibility function
+susceptibility_function <- function(x=999,v=0,delta=0.2,mygamma=0.4){
+  #if(!is.integer(x)){stop("x must be an integer")}
+  if(delta<0 | delta>1){stop("delta must be between 0 and 1 (inclusive)")}
+  if(mygamma<0 | mygamma>1){stop("mygamma must be between 0 and 1 (inclusive)")}
+      
+  if(x==0){rtn <- 0}
+  else if(x>=1 & x<999 & delta==1){rtn <- 0}
+  else if(x>0 & x<(1/delta)) {rtn <- max(x*delta,v*mygamma)}
+  else {rtn <- 1}
+      
+  return(rtn)
+}
+
+# calculate susceptibility
+calc_susceptibility <-function(suscept_mat,x,vac_hist_mat, year, nindiv,delta=0.2,mygamma=0.4){
+  
+  for(i in 1:nindiv){
+    suscept_mat[i,year] <- susceptibility_function(x=x[i,year],v=vac_hist_mat[i,year])
+  }
+  return(suscept_mat[,year])
+}
+  
+### infection probability function
+# a person's probability of infection depends on their susceptibility
+    
+infect <- function(mymybeta=0.15,suscept_mat,year,inf_hist_mat,lastinf){
+  n <- dim(suscept_mat)[1]
+  randnum <-runif(n,0,1)
+  for (i in 1:nindiv){
+    if (randnum[i]<=mymybeta*suscept_mat[i,year]) {
+      inf_hist_mat[i,year] <- 1
+      lastinf[i,year] <- 0
+    }
+    else {inf_hist_mat[i,year] <- 0}
+  }
+  
+  return(list(inf_hist_mat[,year],lastinf))    
+}
+
+### vaccination function
+
+vaccinate_annual <- function(vac_hist_mat,vac_coverage=0.5,year,nindiv,same=FALSE){
+  randnum <-runif(nindiv,0,1)
+  #if(same){ # vaccinate same individuals every year
+  #  
+  #}
+  vac_hist_mat[,year] <- ifelse(randnum<vac_coverage,1,0)
+  return(vac_hist_mat[,year])
+}
+
+vaccinate_biannual <- function(vac_hist_mat,vac_coverage=0.5,year,nindiv){
+  randnum <-runif(nindiv,0,1)
+  tovac <- which(vac_hist_mat[,(year-1)]==0) # only vaccinate people who weren't vaccinated last year
+  vac_hist_mat[tovac,year] <- ifelse(randnum[tovac]<vac_coverage,1,0)
+  return(vac_hist_mat[,year])
+}
+### calculate attack rate
+calculate_ar <- function(inf_hist_mat,nindiv,year){
+  attack_rate <- sum(inf_hist_mat[,year])/nindiv
+  # incidence <- sum(inf_hist_mat[,year])
+  return(ar)
+}
+### Run simulation
+
+nindiv <- 100       # number of individuals
+bins <- 1           # time unit: 1=annual, 2=semi-annual, 4=quarterly, etc.
+startyear <-2010    # start year of simulation
+endyear <- 2018     # end year of simulation
+years <- (endyear-startyear)*bins
+vac_threshold <- 3
+
+# start simulation loop
+run_sim <- function(n=100, years=timepoints,vac_threshold=3,vac_coverage=0.5,mybeta=0.15,delta=0.2,mygamma=0.4,biannual=FALSE){
+  init <- initialize_pop(nindiv=n,timepoints=years)
+  suscept_mat <- init$susceptibility
+  vac_hist_mat <- init$vac_history
+  inf_hist_mat <- init$infection_matrix
+  x <- init$x
+  for (t in 1:years){
+  
+    if (t >= vac_threshold){ 
+      vac_hist_mat[,t] <- vaccinate_annual(vac_hist_mat,year=t,nindiv=n)
+    }
+    if (t<vac_threshold){
+      vac_hist_mat[,t] <- c(rep(0,n))
+    }
+    
+    suscept_mat[,t] <- calc_susceptibility(suscept_mat,x,vac_hist_mat,year=t,nindiv=n)
+  
+    infections <- infect(mybeta, suscept_mat, year=t, inf_hist_mat,lastinf = x)
+    inf_hist_mat[,t] <- infections[[1]]
+    x <- infections[[2]]
+    # update x for next year
+    if (t<years){
+      x[,t+1] <- ifelse(x[,t]<999,x[,t]+1,x[,t])
+    }
+    # calculate attack rate
+    attack_rate[t] <- calculate_ar(inf_hist_mat,n,year=t)
+  }
+  rtn <- list(infection_matrix=inf_hist_mat,x=x,vac_history=vac_hist_mat,susceptibility=suscept_mat,ar=ar)
+  return(rtn)
+}
+
+#out <- run_sim(n=1000, years = 20)
+
