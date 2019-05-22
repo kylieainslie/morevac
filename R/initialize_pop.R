6dfeb612aa5330ee151770fd6820f44bdd6ccfb0
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-05-22 12:12:32 +0100

    make x an array instead of a matrix

--- R/initialize_pop.R
+++ R/initialize_pop.R
@@ -1,7 +1,7 @@
 ### MoReVac - Modelling Repeat Vaccination ###
 ### Agent-based model of repeat vaccination in birth cohort
 
-    
+
 #' Initialize population
 #'
 #' This function initializes the population before running the model.
@@ -12,7 +12,7 @@
 #' @keywords morevac
 #' @export
 #' @examples
-#' initialize_pop()    
+#' initialize_pop()
 initialize_pop <- function(nindiv=1000,years=200,maxage=80){
   # ages
     ages <- sample(0:(maxage-1),nindiv,replace = TRUE)
@@ -20,8 +20,8 @@ initialize_pop <- function(nindiv=1000,years=200,maxage=80){
   # infection status matrix
     inf_stat_mat <- array(NA,dim=c(nindiv,maxage,years))
     colnames(inf_stat_mat) <- c(paste0("Age",0:(maxage-1)))
-  # years since last infection matrix  
-    x <- matrix(c(rep(999, maxage*nindiv)),nrow=nindiv)
+  # years since last infection matrix
+    x <- array(NA,dim=c(nindiv,maxage,years))
     colnames(x) <- c(paste0("Age",0:(maxage-1)))
   # vaccination history matrix
     vac_hist_mat <- array(NA,dim=c(nindiv,maxage,years))
@@ -32,23 +32,23 @@ initialize_pop <- function(nindiv=1000,years=200,maxage=80){
   # infection status matrix
     lifetime_inf <- array(NA,dim=c(nindiv,maxage,years))
     colnames(lifetime_inf) <- c(paste0("Age",0:(maxage-1)))
-    
-    
+
+
   # create naive matrices for population for first year (0 = not infected, NA = not alive)
     for (i in 1:nindiv){
         inf_stat_mat[i,,] <- c(rep(0,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
         vac_hist_mat[i,,] <- c(rep(0,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
          suscept_mat[i,,] <- c(rep(1,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
-                    x[i,] <- c(rep(999,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
+                   x[i,,] <- c(rep(999,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
         lifetime_inf[i,,] <- c(rep(0,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
-    }  
-    
-  # attack rate  
+    }
+
+  # attack rate
     attack_rate <- c(rep(NA,years))
     attack_rate_by_age <- matrix(c(rep(NA,years*maxage)),nrow=years)
     colnames(attack_rate_by_age) <- c(paste0("Age",0:(maxage-1)))
 
     return(list(infection_matrix=inf_stat_mat,x=x,vac_history=vac_hist_mat,
-                susceptibility=suscept_mat,attack_rate=attack_rate, 
+                susceptibility=suscept_mat,attack_rate=attack_rate,
                 attack_rate_by_age=attack_rate_by_age,age=ages,lifetime_infections=lifetime_inf))
 }
