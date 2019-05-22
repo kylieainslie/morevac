5a1f2c862b4fc921035dbb73417fd282ca118a25
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-05-22 14:24:35 +0100

    initialize v

--- R/initialize_pop.R
+++ R/initialize_pop.R
@@ -21,11 +21,14 @@ initialize_pop <- function(nindiv=1000,years=200,maxage=80){
     inf_stat_mat <- array(NA,dim=c(nindiv,maxage,years))
     colnames(inf_stat_mat) <- c(paste0("Age",0:(maxage-1)))
   # years since last infection matrix
-    x <- array(NA,dim=c(nindiv,maxage,years))
+    x <- matrix(c(rep(999, maxage*nindiv)),nrow=nindiv)
     colnames(x) <- c(paste0("Age",0:(maxage-1)))
   # vaccination history matrix
     vac_hist_mat <- array(NA,dim=c(nindiv,maxage,years))
     colnames(vac_hist_mat) <- c(paste0("Age",0:(maxage-1)))
+  # years since vaccination matrix
+    v <- matrix(c(rep(999, maxage*nindiv)),nrow=nindiv)
+    colnames(v) <- c(paste0("Age",0:(maxage-1)))
   # susceptibility matrix
     suscept_mat <- array(1,dim=c(nindiv,maxage,years))
     colnames(suscept_mat) <- c(paste0("Age",0:(maxage-1)))
@@ -39,7 +42,8 @@ initialize_pop <- function(nindiv=1000,years=200,maxage=80){
         inf_stat_mat[i,,] <- c(rep(0,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
         vac_hist_mat[i,,] <- c(rep(0,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
          suscept_mat[i,,] <- c(rep(1,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
-                   x[i,,] <- c(rep(999,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
+                    x[i,] <- c(rep(999,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
+                    v[i,] <- c(rep(999,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
         lifetime_inf[i,,] <- c(rep(0,ages[i]+1),rep(NA,(maxage-1)-ages[i]))
     }
 
@@ -48,7 +52,7 @@ initialize_pop <- function(nindiv=1000,years=200,maxage=80){
     attack_rate_by_age <- matrix(c(rep(NA,years*maxage)),nrow=years)
     colnames(attack_rate_by_age) <- c(paste0("Age",0:(maxage-1)))
 
-    return(list(infection_matrix=inf_stat_mat,x=x,vac_history=vac_hist_mat,
+    return(list(infection_matrix=inf_stat_mat,x=x,vac_history=vac_hist_mat, v=v,
                 susceptibility=suscept_mat,attack_rate=attack_rate,
                 attack_rate_by_age=attack_rate_by_age,age=ages,lifetime_infections=lifetime_inf))
 }
