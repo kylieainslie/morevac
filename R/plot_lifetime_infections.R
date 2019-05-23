4e2b45ff74d8a2f15da382231afe1897f7db1235
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-05-23 12:55:58 +0100

    change by argument to logical by_vac argument

--- R/plot_lifetime_infections.R
+++ R/plot_lifetime_infections.R
@@ -5,26 +5,37 @@
 #'
 #' This function initializes the population before running the model.
 #' @param dat data frame of annual attack rates. Should have two colums: Age and Life_Inf.
-#' @param by Name of variable you want to plot attack rates by.
+#'            If by_vac=TRUE then there should be a third column: Vac_Strategy.
+#' @param by_vac Logical. If true, lifetime infections will be plotted by vaccintion strategy.
 #' @return plot of lifetime infections by age.
 #' @keywords morevac
 #' @export
 
-plot_lifetime_infections <- function(dat, by = NULL){
+plot_lifetime_infections <- function(dat, by_vac = FALSE){
+  if (by_vac){
+    p1 <- ggplot(dat, aes(x = Age, y = Life_Inf,fill = Vac_Strategy)) +
+          geom_boxplot() +
+          ylab('Number of Lifetime Infections') +
+          theme(panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(),
+                axis.line = element_line(colour = "black"),
+                legend.position = c(.25, .95),
+                legend.justification = c("right", "top"),
+                legend.box.just = "right",
+                legend.margin = margin(6, 6, 6, 6),
+                legend.key = element_rect(fill = "white")
+          )
+  } else{
+    p1 <- ggplot(dat, aes(x = Age, y = Life_Inf)) +
+          geom_boxplot() +
+          ylab('Number of Lifetime Infections') +
+          theme(panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(),
+                axis.line = element_line(colour = "black")
+          )
 
-  p1 <- ggplot(dat, aes(x = Age, y = Life_Inf,fill = by)) +
-        geom_boxplot() +
-        ylab('Number of Lifetime Infections') +
-        theme(panel.grid.major = element_blank(),
-              panel.grid.minor = element_blank(),
-              panel.background = element_blank(),
-              axis.line = element_line(colour = "black"),
-              legend.position = c(.25, .95),
-              legend.justification = c("right", "top"),
-              legend.box.just = "right",
-              legend.margin = margin(6, 6, 6, 6),
-              legend.key = element_rect(fill = "white")
-        )
-
+  }
   return(p1)
 }
