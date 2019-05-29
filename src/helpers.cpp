df42ba20363c914c541deb84817ad7ad4d72d6d8
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-05-29 16:54:37 +0100

    create infect_cpp() function

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -14,7 +14,6 @@ using namespace Rcpp;
 
 // Susceptibility function
 //
-// This function initializes the population before running the model.
 // @param inf_history Number years since last infection
 // @param vac_history Number of years since last vaccination
 // @param ve Vaccine efficacy
@@ -25,7 +24,7 @@ using namespace Rcpp;
 // @keywords morevac
 // @export
 // [[Rcpp::export]]
-double suscept_func(int inf_history, int vac_history, double gamma, double drift_x, double drift_v, int version) {
+double suscept_func_cpp(int inf_history, int vac_history, double gamma, double drift_x, double drift_v, int version) {
 
   double vac_ind = 0; // initialize vac_ind
   double rtn = 0;     // initialize return value
@@ -72,5 +71,56 @@ double suscept_func(int inf_history, int vac_history, double gamma, double drift
 }
 
 /*** R
-suscept_func(inf_history = 3, vac_history = 0, gamma = 0.4, drift_x = 0.2, drift_v = 0.2, version = 2)
+suscept_func_cpp(inf_history = 3, vac_history = 0, gamma = 0.4, drift_x = 0.2, drift_v = 0.2, version = 2)
 */
+
+// Infection function
+//
+// This function initializes the population before running the model.
+// @param susceptibility Value of an individual's susceptibility.
+// @param foi Number between 0 and 1 indicating the force of infection.
+// @return Indicator of infection (0 = not infected, 1 = infected).
+// @keywords morevac
+// @export
+// [[Rcpp::export]]
+int infect_cpp(double susceptibility, double foi, double randnum_inf){
+
+  int rtn = 0;
+
+  if (randnum_inf <= foi*susceptibility) {
+    rtn = 1;
+  } else {
+    rtn = 0;
+  }
+  return(rtn);
+}
+
+/*** R
+infect_cpp(susceptibility = 0.5, foi = 0.15, randnum_inf = 0.51)
+*/
+
+
+// Calculate lifetime infections
+//
+// @param a age index
+// @param lifetime_inf number of lifetime infection up to age a
+// @param inf_stat infection status
+// @return number of lifetime infections
+// @keywords morevac
+// @export
+// [[Rcpp::export]]
+int lifetime_infections_cpp(int a, int lifetime_inf, int inf_stat){
+  int rtn = 0;
+  if (a >1){
+    rtn = lifetime_inf + inf_stat;
+  } else if (a==1){
+    rtn = inf_stat;
+  }
+  return(rtn);
+}
+
+/*** R
+lifetime_infections_cpp(a = 1, lifetime_inf = 0, inf_stat = 1)
+lifetime_infections_cpp(a = 5, lifetime_inf = 1, inf_stat = 1)
+  */
+
