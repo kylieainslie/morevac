b88b2cf5b4b37b7710e8518ac3b61c477439e9b8
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-05-31 11:35:08 +0100

    add return_ar_only argument multiannual2 run

--- R/run_sim.R
+++ R/run_sim.R
@@ -47,7 +47,10 @@ run_sim <- function(sim = 100,
     setTxtProgressBar(pb, s)
 
     # run model
-    test <- multiannual2(n = nindiv, vac_coverage = vaccov, suscept_func_version = version, biannual = vac_strat,  rho = rho)
+    test <- multiannual2(n = nindiv, vac_coverage = vaccov,
+                         suscept_func_version = version,
+                         biannual = vac_strat,  rho = rho,
+                         return_ar_only = TRUE)
     # attack rate by age
     out[,,s] <- test$attack_rate_by_age
     dimnames(out)[[1]] <- rownames(test$attack_rate_by_age)
@@ -60,47 +63,7 @@ run_sim <- function(sim = 100,
 
   return(ar_out)
 }
-### output
-  # cohort <- data.frame(Year = c(rep(year_range,3)),
-  #                      Attack_Rate = c(apply(ar_out0,1,mean),
-  #                                      apply(ar_outa,1,mean),
-  #                                      apply(ar_outb,1,mean)),
-  #                      Lower = c(apply(ar_out0,1,FUN = function(x) quantile(x, c(0.025))),
-  #                                apply(ar_outa,1,FUN = function(x) quantile(x, c(0.025))),
-  #                                apply(ar_outb,1,FUN = function(x) quantile(x, c(0.025)))),
-  #                      Upper = c(apply(ar_out0,1,FUN = function(x) quantile(x, c(0.975))),
-  #                                apply(ar_outa,1,FUN = function(x) quantile(x, c(0.975))),
-  #                                apply(ar_outb,1,FUN = function(x) quantile(x, c(0.975)))),
-  #                      Age = c(rep(age_range,3)),
-  #                      Vac_Strategy = c(rep('No Vaccination',length(year_range)),
-  #                                       rep('Annual',length(year_range)),
-  #                                       rep('Biannual',length(year_range)))
-  # )
-  #
-  # p_cohort <- plot_attack_rates(dat = cohort, by_vac = TRUE, c_bands = TRUE)
-  #
-  # pdf(file = paste0(filename,"_ar.pdf"))
-  # plot(p_cohort)
-  # dev.off()
-  # #
-  # # lifetime infections
-  # life_inf_dat <- data.frame(Sim = c(rep(1:sim,3)),
-  #                            Vac_Strategy = c(rep('No Vaccination',sim),
-  #                                             rep('Annual',sim),
-  #                                             rep('Biannual',sim)),
-  #                            rbind(life_inf0,life_infa,life_infb)
-  # )
-  # names(life_inf_dat) <- c('Sim','Vac_Strategy',c(paste0("Age",age_range)))
-  # data_long <- gather(life_inf_dat, Age, Life_Inf, Age0:Age19, factor_key=TRUE)
-  # data_long$Age <- as.factor(str_remove(data_long$Age, 'Age'))
-  #
-  # data_long$Age = with(data_long, reorder(Age, Life_Inf, mean))
-  #
-  # p1 <- plot_lifetime_infections(dat = data_long, by_vac = TRUE)
-  #
-  # pdf(file = paste0(filename,"_life_inf.pdf"))
-  # plot(p1)
-  # dev.off()
+
 
 
 
