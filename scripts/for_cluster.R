0ee1db7a32804a9c38b2027a26298cd59db2f499
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-06-03 14:04:13 +0100

    ...

--- scripts/for_cluster.R
+++ scripts/for_cluster.R
@@ -1,8 +1,8 @@
 ### For cluster
-
+setwd('/Volumes/kainslie')
 # set up your details and which cluster you want (big or small)
 options(didehpc.username = "kainslie",
-        didehpc.cluster = "big")
+        didehpc.cluster = "small")
 
 # dont really know what these lines do but you need them
 context::context_log_start()
@@ -11,26 +11,17 @@ root <- "contexts"
 # setting up the context: what files need to be sourced for your function to work, what packages you need
 # package sources only required here because the package I am using is not on CRAN
 ctx <- context::context_save(path = root,
-                             packages = c("morevac","ggplot2","reshape2","stringr"),
-                             package_sources = provisionr::package_sources(github = "kylieainslie/MoreVac"),
-                             sources = c("functions/likelihood_functions.R",
-                                         "functions/model_functions.R",
-                                         "functions/lazy_mcmc_functions.R",
-                                         "functions/cluster_functions.R"))
-
+                             packages = c("morevac","ggplot2","reshape2","stringr",
+                                          "foreach","parallel","cowplot","doParallel"),
+                             package_sources = provisionr::package_sources(github = "kylieainslie/morevac")
+                             )
 
 # this line should give you a login prompt
 obj <- didehpc::queue_didehpc(ctx)
 
-
-vaccov <- c(0, 0.25, 0.5, 0.75, 1)
 # 'job' is the thing I am running on the cluster
-job <- obj$enqueue(run_sim(sim = 10,
-                           nindiv = 1000,
-                           year_range = c(2000:2019),
-                           age_range = c(0:19))
-                  )
-Collapse
+job1 <- obj$enqueue(sim_for_cluster_func(s = 100, n = 10000, v = 1, r = 0.9))
+job2 <- obj$enqueue(sim_for_cluster_func(s = 100, n = 10000, v = 2, r = 0.5))
 
 
 
