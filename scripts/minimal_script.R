1bbb2c746b3dca6920e6b88f0886042c1785c862
kylieainslie <ainslie.kylie@gmail.com>
2019-06-26 15:02:58 +0100

    plot drift

--- scripts/minimal_script.R
+++ scripts/minimal_script.R
@@ -43,8 +43,18 @@ bc_suscept[2,]
 test <- multiannual2()
 mydat <- test$drift
 mydat$Cum_Drift <- cumsum(mydat$Drift)
+mydat_thinned <- mydat[which(mydat$Vac_Update==1),]
 p_drift <- ggplot(data = mydat, aes(x=Year, y=Cum_Drift)) +
-           geom_line()
+           geom_line() +
+           geom_point(data=mydat_thinned,aes(x=Year,y=Cum_Drift),colour = 'red')
 p_drift
+
+# zoom in on vaccinated years
+mydat_vac <- mydat[181:200,]
+p_vac <- ggplot(data = mydat_vac, aes(x=Year, y=Cum_Drift)) +
+          geom_line() +
+          geom_point(data=mydat_thinned,aes(x=Year,y=Cum_Drift),colour = 'red')
+p_vac
+
 
 
