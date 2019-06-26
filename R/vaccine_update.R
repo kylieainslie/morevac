bbe84e4b8f8d4fcd624620f3e2940d68834add3c
kylieainslie <ainslie.kylie@gmail.com>
2019-06-26 13:34:04 +0100

    change argument names

--- R/vaccine_update.R
+++ R/vaccine_update.R
@@ -1,17 +1,17 @@
 #' Vaccine update function
 #'
 #' This function determines the amount of drift each year.
-#' @param vac_update_status number of years since last vaccine update
+#' @param years_since_vac_update number of years since last vaccine update
 #' @param d distance threshold for vaccine update
-#' @param year_counter year of simulation (from 1 to years)
+#' @param accumulated_drift amount of drift since last vac update
 #' @return binary indicator of whether the vaccine will be updated (0 - no, 1 - yes)
 #' @keywords morevac
 #' @export
-vaccine_update <- function(vac_update_status, accumulated_drift, d = 0.5){
-  if(vac_update_status == 0){
+vaccine_update <- function(years_since_vac_update, accumulated_drift, d = 0.5){
+  if(years_since_vac_update == 0){
     rtn <- 0
     return(rtn)
-  } else if (vac_update_status > 0){
+  } else if (years_since_vac_update > 0){
     if (accumulated_drift > d){
       rtn <- 1
     } else {rtn <- 0}
