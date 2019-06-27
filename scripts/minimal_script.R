7e81b3b2548d47373e9d37089d1f4e267a0eaa27
kylieainslie <ainslie.kylie@gmail.com>
2019-06-27 09:18:46 +0100

    add simulatinon data processing

--- scripts/minimal_script.R
+++ scripts/minimal_script.R
@@ -22,23 +22,6 @@
   inf_dat <- out$history[,,1]
   lifetime_inf <- get_lifetime_inf(inf_history = inf_dat, ages = 1:80, maxage = 80)
 
-### checking version 2
-  test <- multiannual2(vac_coverage = 0.25, suscept_func_version = 2)
-  p_test <- plot_attack_rates(dat = test$attack_rate)
-  inf_hist <- test$history[,,1]
-  vac_hist <- test$history[,,2]
-   suscept <- test$history[,,3]
-
-
-birth_cohort <- which(rownames(vac_hist)==19)
-bc_vac_hist <- vac_hist[birth_cohort,1:20]
-bc_inf_hist <- inf_hist[birth_cohort,1:20]
-bc_suscept <- suscept[birth_cohort,1:20]
-
-bc_vac_hist[2,]
-bc_inf_hist[2,]
-bc_suscept[2,]
-
 # plot drift
 test <- multiannual2()
 mydat <- test$drift
@@ -56,5 +39,28 @@ p_vac <- ggplot(data = mydat_vac, aes(x=Year, y=Cum_Drift)) +
           geom_point(data=mydat_thinned,aes(x=Year,y=Cum_Drift),colour = 'red')
 p_vac
 
+# plot simulated data
+setwd("/Volumes/kainslie/morevac_sims/data")
+
+yearRange = 2000:2019
+ageRange = 0:19
+
+tags1 <- c("vs0vc0r0v1_w_drift","vs1vc0r0v1_w_drift","vs2vc0r0v1_w_drift")                                # filename tags
+sim_out1 <- read_in_sim_data(tags1)                                               # read in sim results files
+dat1 <- process_sim_output(sim_out1, year_range = yearRange, age_range = ageRange) # process sim data
+pa1 <- plot_attack_rates(dat = dat1[[1]], by_vac = TRUE, c_bands = TRUE)          # plot attack rates
+pl1 <- plot_lifetime_infections(dat = dat1[[2]], by_vac = TRUE, x=0.5)            # plot lifetime infections
+
+tags2 <- c("vs0vc50r0v1_w_drift","vs1vc50r0v1_w_drift","vs2vc50r0v1_w_drift")                                # filename tags
+sim_out2 <- read_in_sim_data(tags2)                                               # read in sim results files
+dat2 <- process_sim_output(sim_out2, year_range = yearRange, age_range = ageRange) # process sim data
+pa2 <- plot_attack_rates(dat = dat2[[1]], by_vac = TRUE, c_bands = TRUE)          # plot attack rates
+pl2 <- plot_lifetime_infections(dat = dat2[[2]], by_vac = TRUE, x=0.5)            # plot lifetime infections
+
+tags3 <- c("vs0vc50r0v2_w_drift","vs1vc50r0v2_w_drift","vs2vc50r0v2_w_drift")                                # filename tags
+sim_out3 <- read_in_sim_data(tags3)                                               # read in sim results files
+dat3 <- process_sim_output(sim_out3, year_range = yearRange, age_range = ageRange) # process sim data
+pa3 <- plot_attack_rates(dat = dat3[[1]], by_vac = TRUE, c_bands = TRUE)          # plot attack rates
+pl3 <- plot_lifetime_infections(dat = dat3[[2]], by_vac = TRUE, x=0.5)            # plot lifetime infections
 
 
