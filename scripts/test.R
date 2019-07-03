f25bceb2a68a594e4b99fa14df389b60abd74784
Kylie Ainslie <ainslie.kylie@gmail.com>
2019-07-03 11:41:40 +0100

    Update test.R

--- scripts/test.R
+++ scripts/test.R
@@ -1,7 +1,7 @@
 ### testing script ###
 test <- multiannual2()
 p_test <- plot_attack_rates(dat = test$attack_rate)
-
+p_test
 # check drift and vac update
 mydat <- test$drift
 mydat$Cum_Drift <- cumsum(mydat$Drift)
@@ -25,6 +25,26 @@ vac_hist <- test$history[,,2]
        x <- test$history[,,4]
        v <- test$history[,,5]
 
+# original cohort
+mydat_orig <- mydat[1:20,]
+orig_cohort <- which(rownames(vac_hist)==0)
+oc_vac_hist <- vac_hist[orig_cohort,1:20]
+oc_inf_hist <- inf_hist[orig_cohort,1:20]
+oc_suscept <- suscept[orig_cohort,1:20]
+oc_x <- x[orig_cohort,1:20]
+oc_v <- v[orig_cohort,1:20]
+
+i <- 2
+person <- data.frame(inf_hist = oc_inf_hist[i,],
+                     x = oc_x[i,],
+                     vac_hist = oc_vac_hist[i,],
+                     v = oc_v[i,],
+                     suscept  = oc_suscept[i,],
+                     drift = mydat_orig$Drift,
+                     vac_dist = mydat_orig$Vac_Distance,
+                     update = mydat_orig$Vac_Update)
+person
+# birth cohort
 birth_cohort <- which(rownames(vac_hist)==19)
 bc_vac_hist <- vac_hist[birth_cohort,1:20]
 bc_inf_hist <- inf_hist[birth_cohort,1:20]
@@ -32,7 +52,7 @@ bc_inf_hist <- inf_hist[birth_cohort,1:20]
        bc_x <- x[birth_cohort,1:20]
        bc_v <- v[birth_cohort,1:20]
 
-i <- 1
+i <- 2
 person <- data.frame(inf_hist = bc_inf_hist[i,],
                             x = bc_x[i,],
                      vac_hist = bc_vac_hist[i,],
