950b93c8e456cbb5004cdbd7856b844c9159a6e7
kylieainslie <ainslie.kylie@gmail.com>
2019-07-03 13:00:52 +0100

    remove unnecessary code

--- scripts/minimal_script.R
+++ scripts/minimal_script.R
@@ -44,22 +44,9 @@ setwd("/Volumes/kainslie/morevac_sims/data")
 yearRange = 2000:2019
 ageRange = 0:19
 
-tags1 <- c("vs0vc0r0v1_w_drift","vs1vc0r0v1_w_drift","vs2vc0r0v1_w_drift")                                # filename tags
+tags1 <- c("vs0vc50r09v1_w_drift","vs1vc50r09v1_w_drift","vs2vc50r09v1_w_drift")                                # filename tags
 sim_out1 <- read_in_sim_data(tags1)                                               # read in sim results files
 dat1 <- process_sim_output(sim_out1, year_range = yearRange, age_range = ageRange) # process sim data
 pa1 <- plot_attack_rates(dat = dat1[[1]], by_vac = TRUE, c_bands = TRUE)          # plot attack rates
 pl1 <- plot_lifetime_infections(dat = dat1[[2]], by_vac = TRUE, x=0.5)            # plot lifetime infections
-
-tags2 <- c("vs0vc50r0v1_w_drift","vs1vc50r0v1_w_drift","vs2vc50r0v1_w_drift")                                # filename tags
-sim_out2 <- read_in_sim_data(tags2)                                               # read in sim results files
-dat2 <- process_sim_output(sim_out2, year_range = yearRange, age_range = ageRange) # process sim data
-pa2 <- plot_attack_rates(dat = dat2[[1]], by_vac = TRUE, c_bands = TRUE)          # plot attack rates
-pl2 <- plot_lifetime_infections(dat = dat2[[2]], by_vac = TRUE, x=0.5)            # plot lifetime infections
-
-tags3 <- c("vs0vc50r0v2_w_drift","vs1vc50r0v2_w_drift","vs2vc50r0v2_w_drift")                                # filename tags
-sim_out3 <- read_in_sim_data(tags3)                                               # read in sim results files
-dat3 <- process_sim_output(sim_out3, year_range = yearRange, age_range = ageRange) # process sim data
-pa3 <- plot_attack_rates(dat = dat3[[1]], by_vac = TRUE, c_bands = TRUE)          # plot attack rates
-pl3 <- plot_lifetime_infections(dat = dat3[[2]], by_vac = TRUE, x=0.5)            # plot lifetime infections
-
 
