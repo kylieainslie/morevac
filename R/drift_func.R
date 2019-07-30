a351520f653bcf0099745e6e9d1be9178ed1e946
kylieainslie <ainslie.kylie@gmail.com>
2019-07-30 18:08:10 +0100

    change years to nyears

--- R/drift_func.R
+++ R/drift_func.R
@@ -4,14 +4,14 @@
 #' Drift generating function
 #'
 #' This function determines the amount of drift each year.
-#' @param years number of years to determine drift for
+#' @param nyears number of years to determine drift for
 #' @param rate rate of exponential distribution to draw from
 #' @return vector of scaled drift values for each year
 #' @keywords morevac
 #' @export
-drift_func <- function(years = 200, rate = 1){
+drift_func <- function(nyears = 200, rate = 1){
   # drift for infection
-  draws <- rexp(years, rate = rate)
+  draws <- rexp(nyears, rate = rate)
   # scale draws by maximum value (to put it on a scale of 0 to 1)
   scaled_drift <- draws/max(draws)
   return(scaled_drift)
