3d11781ffe231fb3cf643eb49fa0f99daa9602bb
kylieainslie <ainslie.kylie@gmail.com>
2019-07-30 18:07:32 +0100

    create multiannual and implement new Rcpp version of model

---
+++ R/multiannual.R
@@ -0,0 +1,67 @@
+### MoReVac - Modelling Repeat Vaccination ###
+### Agent-based model of repeat vaccination in birth cohort
+
+#' Multi-annual model of infection and vaccination (version 2)
+#'
+#' This function initializes the population before running the model.
+#' @param n number of individuals to be simulated
+#' @param years number of years to run simulation over
+#' @param maxage maximum age of an individual (removed from population after maxage)
+#' @param start_year year to start simulation
+#' @param vac_start_year year that vaccination starts
+#' @param start_vac_age age at which an individual may be vaccinated
+#' @param stop_vac_age age at which vaccination stops
+#' @param vac_coverage vaccination coverage
+#' @param beta_pandemic force of infection when simulation starts
+#' @param beta_epidemic annual force of infection (after initialization of model)
+#' @param delta_x drift parameter from natural infection
+#' @param delta_v drift parameter from vaccination
+#' @param mygamma protective effect of vaccine
+#' @param suscept_func_version integer indicating which susceptibility version to use.
+#'        1 = either-or, 2 = multiplicative.
+#' @param vac_strategy Integer value indicating frequency of vaccination (1 = annual, 2 = biannual, 3 =triannual,...)
+#' @param rho correlation of vaccination
+#' @param wane amount of protection of vaccine due to waning (0, 1) (inclusive)
+#' @param take percentage of vaccine take (0, 1) (inclusive)
+#' @param seed set random seed
+#' @return list with two elements: 1) a list of infection histories and attack rates and
+#'         2) a plot of annual attack rates by vaccination scenario
+#' @keywords morevac
+#' @export
+multiannual <- function(n = 1000,
+                         years = 1820:2019,
+                         maxage = 80,
+                         start_vac_year = 2000,
+                         start_vac_age = 2,
+                         stop_vac_age = 10,
+                         vac_coverage = 0.5,
+                         beta_pandemic = 0.4,
+                         beta_epidemic = 0.2,
+                         delta_x = NULL,
+                         delta_v = NULL,
+                         vac_protect = 0.7,
+                         suscept_func_version = 1,
+                         vac_strategy = 1,
+                         rho = 0,
+                         wane = 0,
+                         take = 1,
+                         seed = NULL
+){
+
+  # set seed
+    if (!is.null(seed)){set.seed(seed)}
+  # initialize the population
+    ages <- sample(0:maxage-1,n,replace = TRUE)
+    init <- initialize_pop_cpp(n = n, maxage = maxage, ages)
+
+  # determine drift
+    drift <- drift_func(nyears = years, rate = 0.5)
+  # determine vaccine update schedule
+
+  # determine years of vaccination (based on vac_strategy)
+    vac_this_year <- ifelse(years>=start_vac_year & years %% vac_strategy == 0, 1, 0)
+    vac_this_year <- ifelse(is.na(vac_this_year),0,vac_this_year)
+
+
+
+}
