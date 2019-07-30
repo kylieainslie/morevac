4d7d12bb12cf6fc9d2750a2f93d3cc8b3d9edece
kylieainslie <ainslie.kylie@gmail.com>
2019-07-30 18:07:57 +0100

    add ages_mat to keep track of the age of each individual in each year

--- src/initialize_pop_cpp.cpp
+++ src/initialize_pop_cpp.cpp
@@ -4,30 +4,33 @@ using namespace Rcpp;
 
 
 // [[Rcpp::export]]
-List initialize_pop_cpp(int n,int maxage, NumericVector ages) {
+List initialize_pop_cpp(int n,int nyears, NumericVector init_ages) {
 
 // initial array
-  NumericMatrix inf_hist_mat(n, maxage);
-  NumericMatrix vac_hist_mat(n, maxage);
-  NumericMatrix suscept_mat(n, maxage);
-  NumericMatrix x(n, maxage);
-  NumericMatrix v(n, maxage);
+  NumericMatrix inf_hist_mat(n, nyears);
+  NumericMatrix vac_hist_mat(n, nyears);
+  NumericMatrix suscept_mat(n, nyears);
+  NumericMatrix x(n, nyears);
+  NumericMatrix v(n, nears);
+  NumericMatrix ages_mat(n, nyears)
 
 // create naive matrices for population for first year (0 = not infected, NA = not alive)
     for (int i = 0; i < n; ++i){
-      for(int j = 0; j < maxage; ++j){
+      for(int j = 0; j < nyears; ++j){
         if(j <= ages[i]){
           inf_hist_mat(i,j) = 0;   // infection history matrix
           vac_hist_mat(i,j) = 0;   // vaccination history matrix
           suscept_mat(i,j) = 1;    // susceptibility matrix
           x(i,j) = 999;            // time since last infection matrix
           v(i,j) = 999;            // time since last vaccination matrix
+          ages_mat(i,j) = j;       // age matrix (the age of an individual in each year)
         } else {
-          inf_hist_mat(i,j) = 999;   // infection history matrix
-          vac_hist_mat(i,j) = 999;   // vaccination history matrix
-          suscept_mat(i,j) = 999;    // susceptibility matrix
-          x(i,j) = 999;            // time since last infection matrix
-          v(i,j) = 999;            // time since last vaccination matrix
+          inf_hist_mat(i,j) = 999;
+          vac_hist_mat(i,j) = 999;
+          suscept_mat(i,j) = 999;
+          x(i,j) = 999;
+          v(i,j) = 999;
+          ages_mat(i,j) = 999;
         }
       }
     }
@@ -38,6 +41,7 @@ List initialize_pop_cpp(int n,int maxage, NumericVector ages) {
     rtn["suscept_mat"] = suscept_mat;
     rtn["time_since_last_inf"] = x;
     rtn["time_since_last_vac"] = v;
+    rtn["ages_mat"] = ages_mat;
 
     return(rtn);
 }
