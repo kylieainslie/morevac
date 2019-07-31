965608a719c85d78db08fbf32460899c655ecfb4
kylieainslie <ainslie.kylie@gmail.com>
2019-07-31 20:47:39 +0100

    calculate and output v in vaccinate_cpp_2

--- src/vaccinate_cpp_2.cpp
+++ src/vaccinate_cpp_2.cpp
@@ -16,7 +16,8 @@ using namespace Rcpp;
 // @keywords morevac
 // @export
 // [[Rcpp::export]]
-NumericMatrix vaccinate_cpp_2(NumericMatrix vac_hist_mat,
+List vaccinate_cpp_2(NumericMatrix vac_hist_mat,
+                              NumericMatrix v,
                               IntegerMatrix ages_mat,
                               IntegerVector vac_this_year,
                               double vac_cov,
@@ -30,10 +31,17 @@ NumericMatrix vaccinate_cpp_2(NumericMatrix vac_hist_mat,
   int nyears = vac_this_year.size();
   int nindiv = vac_hist_mat.ncol();
 
+//  if (v[i,a]>=999){
+//    new_delta_v <- 1
+//  } else if (v[i,a] == 0){
+//    new_delta_v <- 0
+//  } else {new_delta_v <- min(1,sum(drift[(year_counter-v[i,a]+1):year_counter]))}
+
   for(int j = 0; j < nyears; ++j){
       for (int i = 0; i < nindiv; ++i){
         if (vac_this_year[j] == 0){
           vac_hist_mat(i,j) = 0;
+          if(j > 0){v(i,j) = v(i,j-1) + 1;}
         } else{
       // if it's a vaccination year & individual is old enough -> possibly vaccination
         if (ages_mat(i,j) >= start_vac_age){
@@ -44,20 +52,32 @@ NumericMatrix vaccinate_cpp_2(NumericMatrix vac_hist_mat,
         // vaccinate
           if (randnum_vac[i] <= vac_cov * take){
             vac_hist_mat(i,j) = 1;
-          } else {vac_hist_mat(i,j) = 0;}
-        } else {vac_hist_mat(i,j) = 0;}
+            v(i,j) = 0;
+          } else {
+            vac_hist_mat(i,j) = 0;
+            v(i,j) = v(i,j-1) + 1;
+            }
+        } else {
+          vac_hist_mat(i,j) = 0;
+          v(i,j) = v(i,j-1) + 1;
+          }
       }
-    }
+     }
   }
-  return(vac_hist_mat);
+  List rtn;
+  rtn["vac_hist_mat"] = vac_hist_mat;
+  rtn["v"] = v;
+
+  return(rtn);
 }
 
 /*** R
 init_pop <- initialize_pop_cpp(n = 10, nyears = 10, init_ages = sample(1:9,10,replace=TRUE),max_age = 10)
 vac_this_year <- c(0,0,0,0,0,1,0,1,0,1)
-vaccinate_cpp_2(vac_hist_mat = init_pop$vac_hist_mat, ages_mat = init_pop$ages_mat, vac_this_year = vac_this_year,
-              vac_cov = 0.5, take = 1, rho = 1, randnum_vac = runif(10), start_vac_age = 2, stop_vac_age = 5,
-              vac_strategy = 2)
+vaccinate_cpp_2(vac_hist_mat = init_pop$vac_hist_mat, ages_mat = init_pop$ages_mat,
+                v = init_pop$time_since_last_vac,vac_this_year = vac_this_year, vac_cov = 0.5,
+                take = 1, rho = 1, randnum_vac = runif(10),
+                start_vac_age = 2, stop_vac_age = 5, vac_strategy = 2)
 */
 
 
