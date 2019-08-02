66378a4e97176f4831c04a20055201a4c4b2e20c
kylieainslie <ainslie.kylie@gmail.com>
2019-08-02 09:55:46 +0100

    completed new multiannual function - causes RStudio to crash when infect_cpp_2 is called.

--- R/multiannual.R
+++ R/multiannual.R
@@ -5,18 +5,14 @@
 #'
 #' This function initializes the population before running the model.
 #' @param n number of individuals to be simulated
-#' @param years number of years to run simulation over
-#' @param maxage maximum age of an individual (removed from population after maxage)
-#' @param start_year year to start simulation
-#' @param vac_start_year year that vaccination starts
+#' @param years vector of years to run simulation over (YYYY format)
+#' @param max_age maximum age of an individual (removed from population after max_age)
+#' @param vac_start_year year that vaccination starts (YYYY)
 #' @param start_vac_age age at which an individual may be vaccinated
 #' @param stop_vac_age age at which vaccination stops
 #' @param vac_coverage vaccination coverage
-#' @param beta_pandemic force of infection when simulation starts
-#' @param beta_epidemic annual force of infection (after initialization of model)
-#' @param delta_x drift parameter from natural infection
-#' @param delta_v drift parameter from vaccination
-#' @param mygamma protective effect of vaccine
+#' @param betas vector of force of infection parameters for every year
+#' @param vac_protect protective effect of vaccine
 #' @param suscept_func_version integer indicating which susceptibility version to use.
 #'        1 = either-or, 2 = multiplicative.
 #' @param vac_strategy Integer value indicating frequency of vaccination (1 = annual, 2 = biannual, 3 =triannual,...)
@@ -29,31 +25,27 @@
 #' @keywords morevac
 #' @export
 multiannual <- function(n = 1000,
-                         years = 1820:2019,
-                         max_age = 80,
-                         start_vac_year = 2000,
-                         start_vac_age = 2,
-                         stop_vac_age = 10,
-                         vac_coverage = 0.5,
-                         beta_pandemic = 0.4,
-                         beta_epidemic = 0.2,
-                         delta_x = NULL,
-                         delta_v = NULL,
-                         vac_protect = 0.7,
-                         suscept_func_version = 1,
-                         vac_strategy = 1,
-                         rho = 0,
-                         wane = 0,
-                         take = 1,
-                         seed = NULL
-){
+                        years = 1820:2019,
+                        max_age = 80,
+                        start_vac_year = 2000,
+                        start_vac_age = 2,
+                        stop_vac_age = 10,
+                        vac_coverage = 0.5,
+                        betas = c(0.4,rep(0.2,199)),
+                        vac_protect = 0.7,
+                        suscept_func_version = 2,
+                        vac_strategy = 1,
+                        rho = 0,
+                        wane = 0,
+                        take = 1,
+                        seed = NULL
+                        ){
 
   # set seed
     if (!is.null(seed)){set.seed(seed)}
   # initialize the population
-    init_age_vec <- sample(0:maxage-1,n,replace = TRUE)
-    init_pop <- initialize_pop_cpp(n = n, nyears = length(years),
-                                   init_ages = init_ages_vec, max_age = max_age)
+    init_age_vec <- sample(0:max_age-1,n,replace = TRUE)
+    init_pop <- initialize_pop_cpp(n = n, nyears = length(years), init_ages = init_age_vec, max_age = max_age)
 
   # determine drift
     drift <- drift_func(nyears = length(years), rate = 0.5)
@@ -68,20 +60,33 @@ multiannual <- function(n = 1000,
     vac_this_year <- ifelse(years>=start_vac_year & years %% vac_strategy == 0, 1, 0)
     vac_this_year <- ifelse(is.na(vac_this_year),0,vac_this_year)
   # vaccinate
-    vac <- vaccinate_cpp_2(vac_hist_mat = init_pop$vac_hist_mat,
-                           ages_mat = init_pop$ages_mat,
-                           vac_this_year = vac_this_year,
-                           vac_cov = 0.5, take = 1, rho = 1,
-                           randnum_vac = rn_vac,
-                           start_vac_age = start_vac_age,
-                           stop_vac_age = stop_vac_age,
-                           vac_strategy = vac_strategy)
+    vac_pop <- vaccinate_cpp_2(vac_hist_mat = init_pop$vac_hist_mat,
+                               v = init_pop$time_since_last_vac,
+                               ages_mat = init_pop$ages_mat,
+                               vac_this_year = vac_this_year,
+                               vac_cov = vac_coverage, take = take,
+                               rho = rho, start_vac_age = start_vac_age,
+                               stop_vac_age = stop_vac_age,
+                               vac_strategy = vac_strategy)
 
     # calculate delta_v values
-      delta_v <- find_delta_v(v = vac$v, drift = drift)
+      delta_v <- find_delta_v(v = vac_pop$v, drift = drift)
     # run infection model
-      infect_pop <- infect_cpp_2()
+      infect_pop <- infect_cpp_2(inf_history = init_pop$inf_hist_mat,
+                                 vac_history = vac_pop$vac_hist_mat,
+                                 suscept_mat = init_pop$suscept_mat,
+                                 x = init_pop$time_since_last_inf,
+                                 ages_mat = init_pop$ages_mat,
+                                 drift = drift, delta_v = delta_v,
+                                 gammas = gammas, foi = betas,
+                                 wane_rate = wane,
+                                 version = suscept_func_version)
 
-
+      # return
+      rtn <- list(inf_history = infect_pop,
+                  vac_history = vac_pop,
+                  ages = init_pop$ages_mat
+      )
+      return(rtn)
 
 }
