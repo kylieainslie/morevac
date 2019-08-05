84e4fe6a7d46dc46ed24ef7916a5dd761655037f
kylieainslie <ainslie.kylie@gmail.com>
2019-08-05 16:22:56 +0100

    remove start_vac_age and stop_vac_age

--- src/vaccinate_cpp_2.cpp
+++ src/vaccinate_cpp_2.cpp
@@ -23,8 +23,6 @@ List vaccinate_cpp_2(NumericMatrix vac_hist_mat,
                               NumericVector vac_cov,
                               double take,
                               double rho,
-                              int start_vac_age,
-                              int stop_vac_age,
                               int vac_strategy){
 
   int nyears = vac_this_year.size();
@@ -37,24 +35,18 @@ List vaccinate_cpp_2(NumericMatrix vac_hist_mat,
           vac_hist_mat(i,j) = 0;
           if(j > 0){v(i,j) = v(i,j-1) + 1;}
         } else{
-      // if it's a vaccination year & individual is old enough -> possibly vaccination
-        if (ages_mat(i,j) >= start_vac_age){
         // determine vaccination probability by incorporating prior vaccination
-          if (vac_hist_mat(i,j)-vac_strategy == 1){
-            randnum_vac[i] = randnum_vac[i] * (1-rho);
-          }
+        if (vac_hist_mat(i,j)-vac_strategy == 1){
+          randnum_vac[i] = randnum_vac[i] * (1-rho);
+        }
         // vaccinate
-          if (randnum_vac[i] <= vac_cov[ages_mat(i,j)] * take){
-            vac_hist_mat(i,j) = 1;
-            v(i,j) = 0;
-          } else {
-            vac_hist_mat(i,j) = 0;
-            v(i,j) = v(i,j-1) + 1;
-            }
+        if (randnum_vac[i] <= vac_cov[ages_mat(i,j)+1] * take){
+          vac_hist_mat(i,j) = 1;
+          v(i,j) = 0;
         } else {
           vac_hist_mat(i,j) = 0;
           v(i,j) = v(i,j-1) + 1;
-          }
+        }
       }
      }
   }
@@ -69,7 +61,6 @@ List vaccinate_cpp_2(NumericMatrix vac_hist_mat,
 init_pop <- initialize_pop_cpp(n = 10, nyears = 10, init_ages = sample(1:9,10,replace=TRUE),max_age = 10)
 vac_this_year <- c(0,0,0,0,0,1,0,1,0,1)
 vaccinate_cpp_2(vac_hist_mat = init_pop$vac_hist_mat, ages_mat = init_pop$ages_mat,
-                v = init_pop$time_since_last_vac,vac_this_year = vac_this_year, vac_cov = 0.5,
-                take = 1, rho = 1,
-                start_vac_age = 2, stop_vac_age = 5, vac_strategy = 2)
+                v = init_pop$time_since_last_vac,vac_this_year = vac_this_year,
+                vac_cov = c(rep(0.24,10)),take = 1, rho = 1,vac_strategy = 2)
 */
