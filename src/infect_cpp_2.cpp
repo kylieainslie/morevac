0e8ddb57a2ae7c5d8422af4f0a4d1beed8131430
kylieainslie <ainslie.kylie@gmail.com>
2019-08-06 10:26:45 +0100

    debug - fixed issue with susceptibility not reflecting vaccination

--- src/infect_cpp_2.cpp
+++ src/infect_cpp_2.cpp
@@ -52,8 +52,8 @@ List infect_cpp_2(NumericMatrix inf_history,
           tmp_delta[i] += drift[j];
           delta_x(i,j) = std::min(1.0,tmp_delta[i]);
         }
-      // determine susceptibility
-        if (ages_mat(i,j) == 0 || x(i,j)>=999) {suscept_mat(i,j) = 1;
+      // determine susceptibility in special cases
+        if (ages_mat(i,j) == 0) {suscept_mat(i,j) = 1;
         } else if (x(i,j) == 0){
           suscept_mat(i,j) = 0;
         } else {
@@ -97,21 +97,21 @@ List infect_cpp_2(NumericMatrix inf_history,
 
 /*** R
 # input parameter values
-nyears <- 200
-maxage <- 80
-nindiv <- 1000
+nyears <- 30
+maxage <- 20
+nindiv <- 100
 betas <- c(0.4, rep(0.2,nyears-1))
 # initialize population
 init_age_vec <- sample(0:maxage-1,nindiv,replace=TRUE)
 init_pop <- initialize_pop_cpp(n = nindiv, nyears = nyears, init_ages = init_age_vec, max_age = maxage)
-vac_this_year <- c(rep(0,nyears))
-drift <- drift_func(nyears = nyears, rate = 0.5)
+vac_this_year <- c(rep(0,nyears/2),rep(1,nyears/2))
+drift <- drift_func(nyears = nyears, rate = 0.25)
 # determine vaccine update schedule
 run_update <- vaccine_update_cpp(drift = drift, threshold = 0.5, vac_protect = 0.7)
 gammas <- run_update$gammas
 vac <- vaccinate_cpp_2(vac_hist_mat = init_pop$vac_hist_mat, ages_mat = init_pop$ages_mat,
-                       v = init_pop$time_since_last_vac,vac_this_year = vac_this_year, vac_cov = 0.5,
-                       take = 1, rho = 1, start_vac_age = 2, stop_vac_age = 5, vac_strategy = 2)
+                       v = init_pop$time_since_last_vac,vac_this_year = vac_this_year,
+                       vac_cov = c(rep(1,maxage)),take = 1, rho = 1, vac_strategy = 2)
 delta_v <- find_delta_v(v = vac$v, drift = drift)
 infect_pop <- infect_cpp_2(inf_history = init_pop$inf_hist_mat,
                            vac_history = vac$vac_hist_mat,
@@ -124,4 +124,9 @@ infect_pop <- infect_cpp_2(inf_history = init_pop$inf_hist_mat,
                            foi = betas,
                            wane_rate = 0,
                            version = 2)
+person <- data.frame(Year = 1820:(1820+nyears-1), Vac_History = vac$vac_hist_mat[2,],
+                     Inf_History = infect_pop$inf_hist_mat[2,],
+                     X = infect_pop$x[2,],
+                     Susceptibility = infect_pop$suscept_mat[2,],
+                     Drift = drift, Update = run_update$update)
 */
