829d0b87f26a0ef34352a9ea0009630793091aa0
kylieainslie <ainslie.kylie@gmail.com>
2019-08-06 10:05:53 +0100

    add debugging and bin attack rates by age into age groups (for plotting)

--- scripts/options_results_script.R
+++ scripts/options_results_script.R
@@ -18,15 +18,33 @@ library(stringr)
 vac_cov_dat <- data.frame(Age = 0:79,
                           Same_Vac = c(rep(0.24,80)), # vaccination coverage is 24% for everyone
                           By_Group = c(rep(0.005,2),rep(0.44,2),0.379,
-                                       rep(0.233,11),rep(0.105,49),rep(0.729,15))))
+                                       rep(0.233,11),rep(0.105,49),rep(0.729,15)),
+                          Total_Vac = c(rep(1,80))) # vaccination coverage is 100% for everyone
 # run multi-annual model
-out <- multiannual(n=10000, vac_coverage = 1, stop_vac_age = 18)
+out <- multiannual(n=100000, vac_coverage = vac_cov_dat$Total_Vac)
+
+### Debugging ###
+person <- data.frame(Year = years, Vac_History = vac_history[1,], Susceptibility = suscept_mat[1,],
+                     Inf_History = inf_history[1,], Delta_X = out$inf_history$delta_x[1,])
+
+#################
 # get attack rates
 ar_out <- get_attack_rates(inf_history = out$inf_history$inf_hist_mat,
                            ages_mat = out$ages, years = 1820:2019)
-# outputs
-# plot attack rates
+# plot total attack rates
 p1 <- plot_attack_rates(dat = ar_out$attack_rates)
-# plot lifetime infections
-inf_dat <- out$history[,,1]
-lifetime_inf <- get_lifetime_inf(inf_history = inf_dat, ages = 1:80, maxage = 80)
+
+# bin ages into groups
+age_group_dat <- ar_out$ar_by_age
+age_group_dat$Age <- as.numeric(as.character(age_group_dat$Age))
+age_group_dat$Age_Group <- age_group_dat$Age
+for (i in 1:dim(ar_by_age)[1]){
+  if(age_group_dat$Age[i] >= 0 & age_group_dat$Age[i] < 2){age_group_dat$Age_Group[i] <- '<2'}
+  else if (age_group_dat$Age[i] >= 2 & age_group_dat$Age[i] < 5){age_group_dat$Age_Group[i] <- '2-4'}
+  else if (age_group_dat$Age[i] >= 5 & age_group_dat$Age[i] < 11){age_group_dat$Age_Group[i] <- '5-10'}
+  else if (age_group_dat$Age[i] >= 11 & age_group_dat$Age[i] < 16){age_group_dat$Age_Group[i] <- '11-15'}
+  else if (age_group_dat$Age[i] >= 16 & age_group_dat$Age[i] < 65){age_group_dat$Age_Group[i] <- '16-64'}
+  else {age_group_dat$Age_Group[i] <- '65+'}
+}
+# plot attack rates by age group
+p2 <- plot_attack_rates(dat = age_group_dat, by_age_group = TRUE)
