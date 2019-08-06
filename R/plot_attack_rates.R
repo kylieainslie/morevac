e01788418092e9c57b0cd300a7ef6dd380e15446
kylieainslie <ainslie.kylie@gmail.com>
2019-08-06 10:05:18 +0100

    add by_age_group

--- R/plot_attack_rates.R
+++ R/plot_attack_rates.R
@@ -17,12 +17,12 @@
 #' @keywords morevac
 #' @export
 
-plot_attack_rates <- function(dat, by_vac = FALSE, c_bands = FALSE, y_max = 0.5,
+plot_attack_rates <- function(dat, by_vac = FALSE, by_age_group = FALSE, c_bands = FALSE, y_max = 0.5,
                               no_legend = FALSE, legend_x = 0.95, legend_y = 0.95){
-
+  if(by_vac ==TRUE && by_age_group == TRUE){stop("Please select only one group to plot by (by_vac or by_age_group)")}
   years <- unique(dat$Year)
 
-  if (by_vac == FALSE){
+  if (by_vac == FALSE && by_age_group == FALSE){
     if (c_bands) {
       p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate)) +
             geom_line() +
@@ -45,7 +45,7 @@ plot_attack_rates <- function(dat, by_vac = FALSE, c_bands = FALSE, y_max = 0.5,
                   panel.background = element_blank(),
                   axis.line = element_line(colour = "black"))
         }
-  } else if (by_vac == TRUE) {
+  } else if (by_vac == TRUE && by_age_group == FALSE) {
       if (no_legend) {
         if (c_bands){
           p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Vac_Strategy)) +
@@ -73,11 +73,11 @@ plot_attack_rates <- function(dat, by_vac = FALSE, c_bands = FALSE, y_max = 0.5,
                       legend.position = "none"
                       )
         }
-      } else {
+      } else if (by_vac == FALSE && by_age_group == TRUE) {
         if (c_bands){
-          p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Vac_Strategy)) +
+          p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Age_Group)) +
                 geom_line() +
-                geom_ribbon(aes(x=Year,ymin=Lower,ymax=Upper,linetype=NA,fill=Vac_Strategy),alpha=0.2)+
+                geom_ribbon(aes(x=Year,ymin=Lower,ymax=Upper,linetype=NA,fill=Age_Group),alpha=0.2)+
                 xlab('Year') +
                 ylab('Attack Rate') +
                 scale_y_continuous(limits = c(0,y_max), expand = c(0,0)) +
@@ -91,7 +91,7 @@ plot_attack_rates <- function(dat, by_vac = FALSE, c_bands = FALSE, y_max = 0.5,
                       legend.margin = margin(6, 6, 6, 6),
                       legend.key = element_rect(fill = "white"))
           } else {
-            p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Vac_Strategy)) +
+            p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Age_Group)) +
                   geom_line() +
                   xlab('Year') +
                   ylab('Attack Rate') +
