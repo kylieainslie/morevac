994c13a521568505e355acb6cf63b60a9acb4288
kylieainslie <ainslie.kylie@gmail.com>
2019-08-07 12:49:13 +0100

    fix error in nindiv

--- src/vaccinate_cpp_2.cpp
+++ src/vaccinate_cpp_2.cpp
@@ -17,16 +17,16 @@ using namespace Rcpp;
 // @export
 // [[Rcpp::export]]
 List vaccinate_cpp_2(NumericMatrix vac_hist_mat,
-                              NumericMatrix v,
-                              IntegerMatrix ages_mat,
-                              IntegerVector vac_this_year,
-                              NumericVector vac_cov,
-                              double take,
-                              double rho,
-                              int vac_strategy){
+                     NumericMatrix v,
+                     IntegerMatrix ages_mat,
+                     IntegerVector vac_this_year,
+                     NumericVector vac_cov,
+                     double take,
+                     double rho,
+                     int vac_strategy){
 
   int nyears = vac_this_year.size();
-  int nindiv = vac_hist_mat.ncol();
+  int nindiv = vac_hist_mat.nrow();
 
   for(int j = 0; j < nyears; ++j){
     NumericVector randnum_vac = runif(nindiv);
@@ -36,7 +36,7 @@ List vaccinate_cpp_2(NumericMatrix vac_hist_mat,
           if(j > 0){v(i,j) = v(i,j-1) + 1;}
         } else{
         // determine vaccination probability by incorporating prior vaccination
-        if (vac_hist_mat(i,j)-vac_strategy == 1){
+        if (vac_hist_mat(i,j-vac_strategy) == 1){
           randnum_vac[i] = randnum_vac[i] * (1-rho);
         }
         // vaccinate
@@ -51,6 +51,7 @@ List vaccinate_cpp_2(NumericMatrix vac_hist_mat,
      }
   }
   List rtn;
+  rtn["n"] = nindiv;
   rtn["vac_hist_mat"] = vac_hist_mat;
   rtn["v"] = v;
 
@@ -58,7 +59,7 @@ List vaccinate_cpp_2(NumericMatrix vac_hist_mat,
 }
 
 /*** R
-init_pop <- initialize_pop_cpp(n = 10, nyears = 10, init_ages = sample(1:9,10,replace=TRUE),max_age = 10)
+init_pop <- initialize_pop_cpp(n = 100, nyears = 10, init_ages = sample(1:9,10,replace=TRUE),max_age = 10)
 vac_this_year <- c(1,1,1,1,1,1,1,1,1,1)
 vaccinate_cpp_2(vac_hist_mat = init_pop$vac_hist_mat, ages_mat = init_pop$ages_mat,
                 v = init_pop$time_since_last_vac,vac_this_year = vac_this_year,
