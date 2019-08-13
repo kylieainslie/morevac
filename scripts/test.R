f7089d9e932513493f3ef8616af52d8a53949748
Kylie Ainslie <kainslie@wpia-dide023.dide.ic.ac.uk>
2019-08-13 17:37:00 +0100

    add birth cohort test

--- scripts/test.R
+++ scripts/test.R
@@ -116,11 +116,26 @@ p <- plot_grid(p_vac, p_no_vac_suscept,p_suscept, labels = "AUTO", ncol = 1, ali
 p
 
 ### test multiannual() ###
-test <- multiannual(n = 1000, years = 2000:2019, max_age = 20, vac_coverage = c(rep(0.5,20)),
-                    betas = c(0.4,rep(0.2,19)), start_vac_year = 2000, vac_protect = 0.7, vac_strategy = 2)
+test <- multiannual(n = 10000, years = 1820:2019, max_age = 80, vac_coverage = vac_cov_dat$Fifty_Off_At_10,
+                    betas = c(0.4,rep(0.2,199)), start_vac_year = 2000, vac_protect = 0.7, vac_strategy = 2,
+                    drift_off = FALSE, suscept_func_version = 1)
+bc <- which(test$ages[,181]==0)
+bc_inf_hist <- test$inf_history$inf_hist_mat[bc,181:200]
+bc_vac_hist <- test$vac_history$vac_hist_mat[bc,181:200]
+bc_person <- data.frame(Year = 2000:2019, Age = test$ages[bc[1],181:200],
+                        Vac_History = test$vac_history$vac_hist_mat[bc[1],181:200],
+                        V = test$vac_history$v[bc[1],181:200],
+                        Inf_History = test$inf_history$inf_hist_mat[bc[1],181:200],
+                        X = test$inf_history$x[bc[1],181:200],
+                        Delta_X = test$inf_history$delta_x[bc[1],181:200],
+                        Susceptibility = test$inf_history$suscept_mat[bc[1],181:200],
+                        Drift = test$drift[181:200],
+                        Vac_This_Year = test$vac_this_year[181:200]
+                        )
+
 # get attack rates
-ar_test <- get_attack_rates(inf_history = test$inf_history$inf_hist_mat,
-                           ages_mat = test$ages, years = 2000:2019)
+ar_test <- get_attack_rates(inf_history = bc_inf_hist,
+                           ages_mat = test$ages[bc,181:200], years = 2000:2019)
 # plot total attack rates
 p1 <- plot_attack_rates(dat = ar_test$attack_rates)
 p1
