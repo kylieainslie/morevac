ca956397d4e7e57ebd50554d3cd69bea324994bd
Kylie Ainslie <kainslie@wpia-dide023.dide.ic.ac.uk>
2019-08-15 14:10:17 +0100

    revamp plot_attack_rates()

--- R/plot_attack_rates.R
+++ R/plot_attack_rates.R
@@ -17,99 +17,66 @@
 #' @keywords morevac
 #' @export
 
-plot_attack_rates <- function(dat, by_vac = FALSE, by_age_group = FALSE, c_bands = FALSE, y_max = 0.5,
+plot_attack_rates <- function(dat, by_vac_strategy = FALSE, by_age_group = FALSE, c_bands = FALSE, y_max = 0.5,
                               no_legend = FALSE, legend_x = 0.95, legend_y = 0.95){
-  if(by_vac ==TRUE && by_age_group == TRUE){stop("Please select only one group to plot by (by_vac or by_age_group)")}
   years <- unique(dat$Year)
 
-  if (by_vac == FALSE && by_age_group == FALSE){
-    if (c_bands) {
-      p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate)) +
-            geom_line() +
-            geom_ribbon(aes(x=Year,ymin=Lower,ymax=Upper,linetype=NA),alpha=0.2)+
-            xlab('Year') +
-            ylab('Attack Rate') +
-            scale_y_continuous(limits = c(0,y_max), expand = c(0,0)) +
-            theme(panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(),
-                  panel.background = element_blank(),
-                  axis.line = element_line(colour = "black"))
-    } else {
-      p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate)) +
-            geom_line() +
-            scale_y_continuous(limits = c(0,y_max), expand = c(0,0)) +
-            xlab('Year') +
-            ylab('Attack Rate') +
-            theme(panel.grid.major = element_blank(),
-                  panel.grid.minor = element_blank(),
-                  panel.background = element_blank(),
-                  axis.line = element_line(colour = "black"))
-        }
-  } else if (by_vac == TRUE && by_age_group == FALSE) {
-      if (no_legend) {
-        if (c_bands){
-          p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Vac_Strategy)) +
-                geom_line() +
-                geom_ribbon(aes(x=Year,ymin=Lower,ymax=Upper,linetype=NA,fill=Vac_Strategy),alpha=0.2)+
-                xlab('Year') +
-                ylab('Attack Rate') +
-                scale_y_continuous(limits = c(0,y_max), expand = c(0,0)) +
-                theme(panel.grid.major = element_blank(),
-                      panel.grid.minor = element_blank(),
-                      panel.background = element_blank(),
-                      axis.line = element_line(colour = "black"),
-                      legend.position = "none"
-                     )
-        } else {
-          p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Vac_Strategy)) +
-                geom_line() +
-                xlab('Year') +
-                ylab('Attack Rate') +
-                scale_y_continuous(limits = c(0,y_max), expand = c(0,0)) +
-                theme(panel.grid.major = element_blank(),
-                      panel.grid.minor = element_blank(),
-                      panel.background = element_blank(),
-                      axis.line = element_line(colour = "black"),
-                      legend.position = "none"
-                      )
-        }
-      } else if (by_vac == FALSE && by_age_group == TRUE) {
-        if (c_bands){
-          p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Age_Group)) +
-                geom_line() +
-                geom_ribbon(aes(x=Year,ymin=Lower,ymax=Upper,linetype=NA,fill=Age_Group),alpha=0.2)+
-                xlab('Year') +
-                ylab('Attack Rate') +
-                scale_y_continuous(limits = c(0,y_max), expand = c(0,0)) +
-                theme(panel.grid.major = element_blank(),
-                      panel.grid.minor = element_blank(),
-                      panel.background = element_blank(),
-                      axis.line = element_line(colour = "black"),
-                      legend.position = c(legend_x, legend_y),
-                      legend.justification = c("right", "top"),
-                      legend.box.just = "right",
-                      legend.margin = margin(6, 6, 6, 6),
-                      legend.key = element_rect(fill = "white"))
-          } else {
-            p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Age_Group)) +
-                  geom_line() +
-                  xlab('Year') +
-                  ylab('Attack Rate') +
-                  scale_y_continuous(limits = c(0,y_max), expand = c(0,0)) +
-                  theme(panel.grid.major = element_blank(),
-                        panel.grid.minor = element_blank(),
-                        panel.background = element_blank(),
-                        axis.line = element_line(colour = "black"),
-                        legend.position = c(legend_x, legend_y),
-                        legend.justification = c("right", "top"),
-                        legend.box.just = "right",
-                        legend.margin = margin(6, 6, 6, 6),
-                        legend.key = element_rect(fill = "white"))
-        }
-      }
-
+  if (by_vac_strategy) {
+    p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Vac_Strategy)) +
+          geom_line()
+  } else if (by_age_group) {
+    p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate, colour= Age_Group)) +
+      geom_line()
+  } else {
+    p1 <- ggplot(data = dat, aes(x = Year, y = Attack_Rate)) +
+          geom_line()
   }
 
+  p1 <- p1 + xlab('Year') +
+        ylab('Attack Rate') +
+        scale_y_continuous(limits = c(0,y_max), expand = c(0,0))
+
+  if (c_bands){
+    if (no_legend){
+      p1 <- p1 + geom_ribbon(aes(x=Year,ymin=Lower,ymax=Upper,linetype=NA),alpha=0.2)+
+               theme(panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(),
+                     panel.background = element_blank(),
+                     axis.line = element_line(colour = "black"),
+                     legend.position = "none"
+                     )
+    } else {
+      p1 <- p1 + geom_ribbon(aes(x=Year,ymin=Lower,ymax=Upper,linetype=NA),alpha=0.2)+
+               theme(panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(),
+                     panel.background = element_blank(),
+                     axis.line = element_line(colour = "black"),
+                     legend.position = c(legend_x, legend_y),
+                     legend.justification = c("right", "top"),
+                     legend.box.just = "right",
+                     legend.margin = margin(6, 6, 6, 6),
+                     legend.key = element_rect(fill = "white"))
+    }
+  } else{
+    if (no_legend){
+      p1 <- p1 + theme(panel.grid.major = element_blank(),
+                             panel.grid.minor = element_blank(),
+                             panel.background = element_blank(),
+                             axis.line = element_line(colour = "black"),
+                             legend.position = "none"
+                             )
+    } else {
+      p1 <- p1 + theme(panel.grid.major = element_blank(),
+                             panel.grid.minor = element_blank(),
+                             panel.background = element_blank(),
+                             axis.line = element_line(colour = "black"),
+                             legend.position = c(legend_x, legend_y),
+                             legend.justification = c("right", "top"),
+                             legend.box.just = "right",
+                             legend.margin = margin(6, 6, 6, 6),
+                             legend.key = element_rect(fill = "white"))
+   }
+  }
   return(p1)
 }
 
