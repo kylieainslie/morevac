15c495c0745b8c49790188088b9de0771ac8681f
kylieainslie <ainslie.kylie@gmail.com>
2019-10-24 13:30:54 +0100

    plots plots plots

--- scripts/rolling_cohort_plots.R
+++ scripts/rolling_cohort_plots.R
@@ -20,17 +20,40 @@ p_raster
 
 # bar plot with CI for number of lifetime infections by vac_strategy (binned by number of vacs)
 # need to subset by different parameter values
-# aggregate over all param values
-aggdata <-aggregate(li_dat, by=list(li_dat$Num_Vacs,li_dat$Vac_Strategy),FUN=median, na.rm=TRUE)
-p_point <- ggplot(li_dat, aes(x=Num_Vacs, y=Lifetime_Infs)) +
-  geom_dotplot(
-    aes(fill = Vac_Strategy, color = Vac_Strategy), trim = FALSE,
-    binaxis='y', stackdir='center', dotsize = 0.8,
-    position = position_dodge(0.8)
-  )
-# geom_pointrange(aes(ymin=Lower, ymax=Upper), position = position_dodge(width = 1),)
-p_point
+# remove Lifetime_Infs = NA rows
+li_dat1 <- li_dat[!is.na(li_dat$Lifetime_Infs),]
+li_dat_e01 <- li_dat1[li_dat1$Epsilon <= 0.01,]
+li_dat_e02 <- li_dat1[li_dat1$Epsilon > 0.01 & li_dat1$Epsilon <= 0.02,]
+li_dat_e03 <- li_dat1[li_dat1$Epsilon > 0.02 & li_dat1$Epsilon <= 0.03,]
+li_dat_e04 <- li_dat1[li_dat1$Epsilon > 0.03 & li_dat1$Epsilon <= 0.04,]
+li_dat_e05 <- li_dat1[li_dat1$Epsilon > 0.04 & li_dat1$Epsilon <= 0.05,]
 
+# aggregate over all param values
+aggdata <-aggregate(li_dat_e01[,-c(1,2)], by=list(li_dat_e01$Num_Vacs,li_dat_e01$Vac_Strategy),FUN=mean, na.rm=TRUE)
+names(aggdata)[1:2] <- c("Num_Vacs","Vac_Strategy")
+# bar chart would be clearer
+# only show a few num vac categories, not all of them!
+
+# p_point <- ggplot(aggdata, aes(x=as.factor(Num_Vacs), y=Lifetime_Infs)) +
+#   geom_dotplot(aes(fill = Vac_Strategy, color = Vac_Strategy),
+#                binaxis='y', stackdir='center', dotsize = 0.8,
+#                position = position_dodge(0.8), binwidth = 0.05)
+# p_point
+
+p_bar<- ggplot(li_dat_e04, aes(x=Num_Vacs, y=Lifetime_Infs, fill=Vac_Strategy)) +
+        geom_bar(stat="identity", color="black", position=position_dodge()) +
+        geom_errorbar(aes(ymin=Lower, ymax=Upper), width=.2,position=position_dodge(.9))
+p_bar
+
+p_bar <- p_bar + theme(panel.grid.major = element_blank(),
+                 panel.grid.minor = element_blank(),
+                 panel.background = element_blank(),
+                 axis.line = element_line(colour = "black"),
+                 legend.position = c(legend_x, legend_y),
+                 legend.justification = c("right", "top"),
+                 legend.box.just = "right",
+                 legend.margin = margin(6, 6, 6, 6),
+                 legend.key = element_rect(fill = "white"))
 # plot results
 y_max <- 0.4
 legend_x <- 0.95
