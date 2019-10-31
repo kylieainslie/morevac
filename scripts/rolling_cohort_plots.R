6ff49a433fe907fce2e936e2ae258a64b12bed20
kylieainslie <ainslie.kylie@gmail.com>
2019-10-31 08:15:58 +0000

    tidying up

--- scripts/rolling_cohort_plots.R
+++ scripts/rolling_cohort_plots.R
@@ -41,12 +41,16 @@ p_point <- ggplot(li_dat, aes(x=Num_Vacs, y=Lifetime_Infs)) +
 #########################
 ### attack rate plots ###
 #########################
-# round numeric variables to 2 decimal places
-#ar_dat[,-c(1,2)] <- round(ar_dat[,-c(1,2)],2)
+# create AR difference and AR ratio variables
+dt_ar_sub <- dt_ar[dt_ar$Vac_Strategy == "Annual",]
+dt_ar_sub$Diff <- dt_ar[dt_ar$Vac_Strategy == "Annual",3] - dt_ar[dt_ar$Vac_Strategy == "Every Other Year",3]
+dt_ar_sub$ Ratio <- dt_ar[dt_ar$Vac_Strategy == "Annual",3] / dt_ar[dt_ar$Vac_Strategy == "Every Other Year",3]
+
+### heatmap
+# for better heatmap resolution, round parameter values
 dt_ar2 <- cbind(dt_ar[,1:2],round(dt_ar[,c(3:11)],2)) # round to 2 decimal places
 dt_ar3 <- cbind(dt_ar[,1:3],round(dt_ar[,c(3:11)],1)) # round to 1 decimal place
 
-### heatmap
 p_heat2 <- ggplot(dt_ar2[dt_ar2$Vac_Strategy!="No Vaccination"], aes(x = VE, y = Vac_Cov, fill = Attack_Rate)) +
   geom_tile(alpha=0.2) +
   viridis::scale_fill_viridis() +
@@ -63,10 +67,6 @@ dev.off()
 
 ### heatmap using ratio: annual AR / biennial AR
 # create new ratio variable
-dt_ar2_sub1 <- dt_ar2[dt_ar2$Vac_Strategy == "Annual",]
-dt_ar2_sub2 <- dt_ar2[dt_ar2$Vac_Strategy == "Every Other Year",]
-dt_ar2_sub1$Ratio <- round(dt_ar2_sub1$Attack_Rate/dt_ar2_sub2$Attack_Rate,2)
-
 p_heat_ratio <- ggplot(dt_ar2_sub1, aes(x = VE, y = Waning, fill = Ratio)) +
   geom_tile(alpha=0.2) +
   viridis::scale_fill_viridis() +
@@ -80,10 +80,13 @@ p_heat_ratio
 # create group variable for each unique set of parameter values
 library(dplyr)
 dt_ar <- dt_ar %>%
-         mutate(ID = group_indices(., Vac_Cov, Waning,Take,Epsilon,Rho,VE))
+         mutate(ID = group_indices(., Vac_Cov, Waning,Take,Epsilon,Rho,VE),
+                EpsilonGroup = cut(Epsilon, c(0,0.1,0.2,0.3,0.4,0.5),
+                                   labels = c("<0.1","[0.1,0.2)","[0.2,0.3)","[0.3,0.4)","[0.4,0.5)")))
+
 p_spaghetti <- ggplot(dt_ar, aes(x = Age, y = Attack_Rate, group = ID, color = ID)) +
                geom_line() + viridis::scale_color_viridis() +
-               facet_grid(Vac_Strategy ~ .) +
+               facet_grid(EpsilonGroup ~ Vac_Strategy) +
                theme(legend.position = "none")
 p_spaghetti
 
@@ -91,31 +94,35 @@ png(file = "ar_spaghetti.png")
 p_spaghetti
 dev.off()
 
-## single variable plots
-y_max <- 0.3
-legend_x <- 0.95
-legend_y <- 0.95
-
-p1 <- ggplot(data = dt_ar2, aes(x = VE, y = Attack_Rate, colour= Vac_Strategy)) +
-  geom_point() +
-  #geom_ribbon(aes(x=Age,ymin=Lower,ymax=Upper,linetype=NA, fill = Vac_Strategy),alpha=0.2)+
-  xlab('VE') +
-  ylab('Attack Rate') +
-  scale_y_continuous(limits = c(0,y_max), expand = c(0,0)) +
-  facet_wrap(~Age)
+### univariate scatter plots
+p1 <- ggplot(data = dt_ar_sub, aes(x = VE, y = Diff,color = Diff)) +
+      geom_point() +
+      viridis::scale_color_viridis() +
+      geom_hline(yintercept = 0) +
+      #xlab('VE') +
+      ylab('Difference in Attack Rate') +
+      #scale_x_continuous(labels = c(0,.01,.02,.03,.04,.05))+
+      facet_wrap(~Age)
 p1
 
-p1 <- p1 + theme(panel.grid.major = element_blank(),
-                 panel.grid.minor = element_blank(),
-                 panel.background = element_blank(),
-                 axis.line = element_line(colour = "black"),
-                 legend.position = c(legend_x, legend_y),
-                 legend.justification = c("right", "top"),
-                 legend.box.just = "right",
-                 legend.margin = margin(6, 6, 6, 6),
-                 legend.key = element_rect(fill = "white"))
+png(file = "ar_diff_scatter_ve.png", width = 10, height = 8,
+    units = "in", pointsize = 8, res = 300)
+p1
+dev.off()
+
+### bivariate scatter plots
+p2 <- ggplot(data = dt_ar_sub, aes(x = VE, y = Take,color = Diff)) +
+      geom_point() +
+      viridis::scale_color_viridis() +
+      facet_wrap(~Age)
 p1
 
+png(file = "ar_diff_scatter_ve+take.png", width = 10, height = 8,
+    units = "in", pointsize = 8, res = 300)
+p1
+dev.off()
+
+### Miscallaneous
 legend_x <- 0.25
 legend_y <- 0.95
 tmp$Num_Vacs <- factor(tmp$Num_Vacs)
@@ -139,3 +146,14 @@ p2
 png(file = "rolling_cohort_off_at_10_sim500.png")
 p1
 dev.off()
+
+# p1 <- p1 + theme(panel.grid.major = element_blank(),
+#                  panel.grid.minor = element_blank(),
+#                  panel.background = element_blank(),
+#                  axis.line = element_line(colour = "black"),
+#                  legend.position = c(legend_x, legend_y),
+#                  legend.justification = c("right", "top"),
+#                  legend.box.just = "right",
+#                  legend.margin = margin(6, 6, 6, 6),
+#                  legend.key = element_rect(fill = "white"))
+# p1
