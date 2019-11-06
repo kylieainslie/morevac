17321db94436acecbcd27afa130b7f449f215479
Kylie Ainslie <kainslie@wpia-dide023.dide.ic.ac.uk>
2019-11-06 17:36:24 +0000

    add histogram of difference

--- scripts/rolling_cohort_plots.R
+++ scripts/rolling_cohort_plots.R
@@ -20,19 +20,33 @@ setwd("~/Dropbox/Kylie/Projects/morevac_manuscript/figures")
 dt_li2 <- cbind(dt_li[,1:2],round(dt_li[,c(6:11)],2))
 #dt_li[,-c(1,2)] <- round(dt_li[,-c(1,2)],2)
 
-### univariate scatter plots
-fully_vac <- dt_li[(dt_li$Num_Vacs == 5 & dt_li$Vac_Strategy == "Every Other Year")
-                       | (dt_li$Num_Vacs == 9 & dt_li$Vac_Strategy == "Annual"),]
+### characterising difference in lifetime infections between annual and every other year
+diff_sub <- dt_li[dt_li$Vac_Strategy == "Annual",]
 
-p1 <- ggplot(data = fully_vac, aes(x = Epsilon, y = Lifetime_Infs,color = Lifetime_Infs)) +
+p_hist <- ggplot(data = diff_sub, aes(Diff, fill = cut(Diff,100))) +
+          geom_histogram(bins = 50, show.legend = FALSE) +
+          scale_fill_viridis_d() +
+          labs(x = "Distance", y = "Frequency") +
+          theme(panel.grid.major = element_blank(),
+                panel.grid.minor = element_blank(),
+                panel.background = element_blank(),
+                axis.line = element_line(colour = "black"))
+p_hist
+### univariate scatter plots
+#fully_vac <- dt_li[(dt_li$Num_Vacs == 5 & dt_li$Vac_Strategy == "Every Other Year")
+#                       | (dt_li$Num_Vacs == 9 & dt_li$Vac_Strategy == "Annual"),]
+
+p1 <- ggplot(data = diff_sub, aes(x = Waning, y = Diff, color = Diff)) +
   geom_point() +
+  geom_errorbar(aes(ymin=Diff_Lower, ymax=Diff_Upper)) +
   viridis::scale_color_viridis() +
-  facet_grid(. ~ Vac_Strategy) +
-  xlab('Exposure Penalty') +
-  ylab('Lifetime Infections')
+  #facet_grid(. ~ Vac_Strategy) +
+  geom_hline(yintercept = 0, color = "black") +
+  #xlab('VE') +
+  ylab('Difference')
 p1
 
-png(file = "li_scatter_epsilon.png", width = 10, height = 8,
+png(file = "li_scatter_ve.png", width = 10, height = 8,
     units = "in", pointsize = 8, res = 300)
 p1
 dev.off()
@@ -96,7 +110,8 @@ p_heat_ratio <- ggplot(dt_ar2_sub1, aes(x = VE, y = Waning, fill = Ratio)) +
   facet_wrap(~Age) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
-        panel.background = element_blank())
+        panel.background = element_blank(),
+        )
 p_heat_ratio
 
 ### spaghetti plot
