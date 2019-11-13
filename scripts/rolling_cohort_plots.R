72b25f57ab5026281e9375b45979a672e3b3d049
kylieainslie <ainslie.kylie@gmail.com>
2019-11-13 10:46:12 +0000

    scatter with marginal

--- scripts/rolling_cohort_plots.R
+++ scripts/rolling_cohort_plots.R
@@ -3,29 +3,34 @@
 ################################
 ### read in output files
 library(data.table)
-setwd("~/Dropbox/Kylie/Projects/morevac_manuscript/data/attack_rates")
+setwd("~/Dropbox/Kylie/Projects/Morevac/data/attack_rates")
 files1 = list.files(pattern="*.csv")
 dt_ar = do.call(rbind, lapply(files1, fread))[,-1]
 
-setwd("~/Dropbox/Kylie/Projects/morevac_manuscript/data/lifetime_infs/")
+setwd("~/Dropbox/Kylie/Projects/Morevac/data/lifetime_infs/mean/vac_off_at_16_sim10")
 files2 <- list.files(pattern="*.csv")
 dt_li = do.call(rbind, lapply(files2, fread))[,-1]
 
 ### set working directory to save figures in
-setwd("~/Dropbox/Kylie/Projects/morevac_manuscript/figures")
+#setwd("~/Dropbox/Kylie/Projects/Morevac/figures")
 
 ################################
 ### lifetime infection plots ###
 ################################
+setwd("~/Dropbox/Kylie/Projects/Morevac/figures/lifetime_infections/vac_off_at_16/")
+
 #dt_li2 <- cbind(dt_li[,1:2],round(dt_li[,c(6:11)],2))
 #dt_li[,-c(1,2)] <- round(dt_li[,-c(1,2)],2)
 
 ### characterising difference in lifetime infections between annual and every other year
-dt_li2 <- dt_li[dt_li$Epsilon < 0.1,]
-diff_sub <- dt_li2[dt_li2$Vac_Strategy == "Annual",]
-
+# dt_li2 <- dt_li[dt_li$Epsilon < 0.1,]
+diff_sub <- dt_li[dt_li$Vac_Strategy == "Annual",]
+# color by diff conf interval values (<0 blue, 0 black, >0 green)
+diff_sub$Diff_Color <- ifelse(diff_sub$Diff_Upper < 0, '<0',
+                              ifelse(diff_sub$Diff_Lower <=0 & diff_sub$Diff_Upper >=0, 'zero',
+                                     ifelse(diff_sub$Diff_Lower >0, '>0', 'something else')))
 p_hist <- ggplot(data = diff_sub, aes(Diff, fill = cut(Diff,100))) +
-          geom_histogram(bins = 50, show.legend = FALSE) +
+          geom_histogram(bins = 30, show.legend = FALSE) +
           scale_fill_viridis_d() +
           labs(x = "Difference", y = "Frequency") +
           theme(panel.grid.major = element_blank(),
@@ -34,9 +39,6 @@ p_hist <- ggplot(data = diff_sub, aes(Diff, fill = cut(Diff,100))) +
                 axis.line = element_line(colour = "black"))
 p_hist
 ### univariate scatter plots
-#fully_vac <- dt_li[(dt_li$Num_Vacs == 5 & dt_li$Vac_Strategy == "Every Other Year")
-#                       | (dt_li$Num_Vacs == 9 & dt_li$Vac_Strategy == "Annual"),]
-
 p_ve <- ggplot(data = diff_sub, aes(x = VE, y = Diff, color = Diff)) +
   geom_point() +
   geom_errorbar(aes(ymin=Diff_Lower, ymax=Diff_Upper)) +
@@ -89,34 +91,34 @@ dev.off()
 
 ### bivariate plots
 #
-p_epsilon_waning <- ggplot(data = diff_sub, aes(x = Epsilon, y = Waning, color = Diff)) +
+p_ve_waning <- ggplot(data = diff_sub, aes(x = VE, y = Waning, color = Diff)) +
   geom_point() +
   viridis::scale_color_viridis() +
-  xlab('Exposure Penalty') +
+  #xlab('Exposure Penalty') +
   ylab('Waning')
-p_epsilon_take <- ggplot(data = diff_sub, aes(x = Epsilon, y = Take, color = Diff)) +
+p_ve_take <- ggplot(data = diff_sub, aes(x = VE, y = Take, color = Diff)) +
   geom_point() +
   viridis::scale_color_viridis() +
-  xlab('Exposure Penalty')
-p_epsilon_ve <- ggplot(data = diff_sub, aes(x = Epsilon, y = VE, color = Diff)) +
+  #xlab('Exposure Penalty')
+p_ve_epsilon <- ggplot(data = diff_sub, aes(x = VE, y = Epsilon, color = Diff)) +
   geom_point() +
   viridis::scale_color_viridis() +
-  xlab('Exposure Penalty')
-p_epsilon_rho <- ggplot(data = diff_sub, aes(x = Epsilon, y = Rho, color = Diff)) +
+  ylab('Exposure Penalty')
+p_ve_rho <- ggplot(data = diff_sub, aes(x = VE, y = Rho, color = Diff)) +
   geom_point() +
   viridis::scale_color_viridis() +
-  xlab('Exposure Penalty') +
+  #xlab('Exposure Penalty') +
   ylab('Correlation of Vaccination')
-p_epsilon_vaccov <- ggplot(data = diff_sub, aes(x = Epsilon, y = Vac_Cov, color = Diff)) +
+p_ve_vaccov <- ggplot(data = diff_sub, aes(x = VE, y = Vac_Cov, color = Diff)) +
   geom_point() +
   viridis::scale_color_viridis() +
-  xlab('Exposure Penalty') +
+  #xlab('Exposure Penalty') +
   ylab('Vaccination Coverage')
 
 theme_set(theme_cowplot(font_size=10)) # reduce default font size
 p_all2 <- plot_grid(p_epsilon_waning, p_epsilon_ve,p_epsilon_take,p_epsilon_vaccov, p_epsilon_rho, labels = "AUTO", ncol = 3, align = 'v', axis = 'l')
 
-png(file = "li_diff_bivariate_epsilon.png", width = 10, height = 8,
+png(file = "li_diff_bivariate_ve.png", width = 10, height = 8,
     units = "in", pointsize = 8, res = 300)
 p_all2
 dev.off()
@@ -129,10 +131,32 @@ p_nv <- ggplot(dt_li2, aes(x = VE, y = Lifetime_Infs, color = Vac_Strategy)) +
           xlab('Vaccine Effectiveness') +
           facet_wrap(~Num_Vacs)
 p_nv
+
+### scatter plot with marginal histogram
+p <- ggplot(diff_sub, aes(x = Epsilon, y = Diff,color = Diff_Color)) +
+      geom_point() +
+      geom_errorbar(aes(ymin = Diff_Lower, ymax = Diff_Upper)) +
+      geom_hline(yintercept = 0, color = "black") +
+      xlab("Exposure Penalty") +
+      theme(legend.position = "bottom")
+p_marg <- ggExtra::ggMarginal(p, type = "histogram", margins = "y")
+
+png(file = "li_diff_w_marginal_epsilon.png", width = 10, height = 8,
+    units = "in", pointsize = 8, res = 300)
+p_marg
+dev.off()
+
+### plot fully vaccinated individuals
+max_vac_annual <- max(dt_li[dt_li$Vac_Strategy == "Annual",]$Num_Vacs)
+max_vac_biennial <- max(dt_li[dt_li$Vac_Strategy == "Every Other Year",]$Num_Vacs)
+
+fully_vac <- dt_li[(dt_li$Num_Vacs == max_vac_biennial & dt_li$Vac_Strategy == "Every Other Year")
+                   | (dt_li$Num_Vacs == max_vac_annual & dt_li$Vac_Strategy == "Annual"),]
+
 ### bar chart
 #   with CI for number of lifetime infections by vac_strategy (binned by number of vacs)
 # aggregate over all param values
-aggdata <-aggregate(li_dat, by=list(li_dat$Num_Vacs,li_dat$Vac_Strategy),FUN=median, na.rm=TRUE)
+aggdata <-aggregate(fully_vac, by=list(fully_vac$Num_Vacs,fully_vac$Vac_Strategy),FUN=mean, na.rm=TRUE)
 p_point <- ggplot(li_dat, aes(x=Num_Vacs, y=Lifetime_Infs)) +
   geom_dotplot(
     aes(fill = Vac_Strategy, color = Vac_Strategy), trim = FALSE,
