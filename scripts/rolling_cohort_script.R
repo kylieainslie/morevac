1356fc96ce16fa5239cd60a0e05eb75670be06fd
Kylie Ainslie <kainslie@wpia-dide023.dide.ic.ac.uk>
2019-11-20 18:00:14 +0000

    remove bootstrapping add writing raw data to file

--- scripts/rolling_cohort_script.R
+++ scripts/rolling_cohort_script.R
@@ -12,10 +12,13 @@ library(dplyr)
 library(plyr)
 library(lhs)
 library(boot)
+library(data.table)
 # load morevac package
 setwd("~/Documents/morevac")
 devtools::load_all()
 #######################################
+# specify save directory
+setwd("~/Dropbox/Kylie/Projects/Morevac/data/sim_data")
 ### simulation
 ## create latin hypercube of parameter values to simulate over
 set.seed(1234)
@@ -23,10 +26,11 @@ set.seed(1234)
 mylhc <- randomLHS(1000, 6)
 colnames(mylhc) <- c("Vac_Cov", "Waning", "Take", "Epsilon", "Rho", "VE")
 mylhc[, "Epsilon"] <- qunif(mylhc[,"Epsilon"], min = 0.001, max = 0.1)
-# mylhc[, "Vac_Cov"] <- qunif(mylhc[,"Vac_Cov"], min = 0.20, max = 0.5) # range from 2018-2019 PHE estimates of VC in school-aged children: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/806289/Childhood_flu_annual_report_2018_19_FINAL_.pdf
-# mylhc[, "Take"] <- qunif(mylhc[,"Take"], min = 0.5, max = 1)
+mylhc[, "Vac_Cov"] <- qunif(mylhc[,"Vac_Cov"], min = 0, max = 0.5) # range from 2018-2019 PHE estimates of VC in school-aged children: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/806289/Childhood_flu_annual_report_2018_19_FINAL_.pdf
+mylhc[, "Take"] <- qunif(mylhc[,"Take"], min = 0.5, max = 1)
+# write hyper cube parameters to file
+write.csv(mylhc,file = "latin_hyper_cube_parameters.csv")
 # run simulations
-setwd("~/Dropbox/Kylie/Projects/morevac_manuscript/data")
 # parallelize
 bins <- list(seq(1,100),seq(101,200),seq(201,300),seq(301,400),seq(401,500),
              seq(501,600),seq(601,700),seq(701,800),seq(801,900),seq(901,1000))
@@ -34,8 +38,9 @@ bins <- list(seq(1,100),seq(101,200),seq(201,300),seq(301,400),seq(401,500),
 # ncl <- detectCores()
 # cl <- makeCluster(ncl)
 # registerDoParallel(cl)
+
 for (b in 1:10){
-#foreach(j=1:5, .packages = c('morevac','Rcpp')) %dopar% {
+#foreach(b=1:10, .packages = c('morevac','Rcpp')) %dopar% {
 
 loop_length <- length(bins[[b]])
 for (i in 1:loop_length){
@@ -63,67 +68,49 @@ cat("\n Every other year vaccination simulation running... \n")
 sim_test2 <- run_sim_2(sim = n_sim, n = nindiv, years = myyears, betas = mybetas, vac_cov = vac_cov_dat$Biennial, vac_strategy = 2,
                        wane = mylhc[row_lhc, "Waning"], take = mylhc[row_lhc, "Take"], epsilon = mylhc[row_lhc, "Epsilon"], vac_protect = mylhc[row_lhc, "VE"], rho = mylhc[row_lhc, "Rho"])
 
-# post process sim results
+# extract cohorts from each sim and combine raw inf and vac histories for every simulation
 sim0_results <- postprocess_sim_results_for_rolling_cohort(simdat = sim_test0, total_year_range = myyears, nsim = n_sim)
 sim1_results <- postprocess_sim_results_for_rolling_cohort(simdat = sim_test1, total_year_range = myyears, nsim = n_sim)
 sim2_results <- postprocess_sim_results_for_rolling_cohort(simdat = sim_test2, total_year_range = myyears, nsim = n_sim)
 
-cat("\n Bootstrapping... \n")
-# bootstrapping
-myAR0 <- boot_ar(dat = sim0_results$Attack_Rate_Median, vac_strategy = "No Vaccination", stat = "mean")
-myAR1 <- boot_ar(dat = sim1_results$Attack_Rate_Median, vac_strategy = "Annual", stat = "mean")
-myAR2 <- boot_ar(dat = sim2_results$Attack_Rate_Median, vac_strategy = "Every Other Year", stat = "mean")
+# combine into one data.table
+inf_histories <- rbindlist(list(No_Vac = sim0_results$inf_history, Annual = sim1_results$inf_history, Biennial = sim2_results$inf_history), idcol = 'Vac_Strategy')
+vac_histories <- rbindlist(list(No_Vac = sim0_results$vac_history, Annual = sim1_results$vac_history, Biennial = sim2_results$vac_history), idcol = 'Vac_Strategy')
 
-# combine AR sim results into single data set to plot
-### median dataset
-dat <- rbind(myAR0, myAR1, myAR2)
-dat$Vac_Cov <- mylhc[row_lhc, "Vac_Cov"]
-dat$Waning <- mylhc[row_lhc, "Waning"]
-dat$Take = mylhc[row_lhc, "Take"]
-dat$Epsilon = mylhc[row_lhc, "Epsilon"]
-dat$Rho = mylhc[row_lhc, "Rho"]
-dat$VE = mylhc[row_lhc, "VE"]
+# add parameter value index column
+inf_histories$Param_Index <- row_lhc
+vac_histories$Param_Index <- row_lhc
 
-# bootstrapping
-myLI0 <- boot_li(dat = sim0_results$Lifetime_Infections, vac_strategy = "No Vaccination", stat = "mean")
-myLI1 <- boot_li(dat = sim1_results$Lifetime_Infections, vac_strategy = "Annual", stat = "mean")
-myLI2 <- boot_li(dat = sim2_results$Lifetime_Infections, vac_strategy = "Every Other Year", stat = "mean")
+# write raw output to file
+file <- paste0("sim_",row_lhc)
+try(data.table::fwrite(inf_histories, file = paste0("infection_histories/",file,"_inf_hist.csv"), col.names = TRUE,
+                       row.names = FALSE, sep = ","))
+try(data.table::fwrite(vac_histories, file = paste0("vaccination_histories/",file,"_vac_hist.csv"), col.names = TRUE,
+                       row.names = FALSE, sep = ","))
 
-# bootstrap difference
-li_diff <- sim1_results$Total_Lifetime_Infections$total_mean - sim2_results$Total_Lifetime_Infections$total_mean
-boot_diff <- boot(li_diff, statistic = function(x, d) {return(mean(x[d]))}, R = 1000)
-diff_lower <- boot.ci(boot_diff, index=1, type='perc')$percent[4]
-diff_upper<- boot.ci(boot_diff, index=1, type='perc')$percent[5]
-
-# combine LI sim results into single data set to plot
-dat2 <- rbind(myLI0, myLI1, myLI2)
-dat2$Diff <- boot_diff$t0
-dat2$Diff_Lower <- diff_lower
-dat2$Diff_Upper <- diff_upper
-dat2$Vac_Cov <- mylhc[row_lhc, "Vac_Cov"]
-dat2$Waning <- mylhc[row_lhc, "Waning"]
-dat2$Take = mylhc[row_lhc, "Take"]
-dat2$Epsilon = mylhc[row_lhc, "Epsilon"]
-dat2$Rho = mylhc[row_lhc, "Rho"]
-dat2$VE = mylhc[row_lhc, "VE"]
-
-if (i == 1){
-   ar_out <- dat
-   li_out <- dat2
-} else {
-   ar_out <- rbind(ar_out, dat)
-   li_out <- rbind.fill(li_out, dat2)
-}
-}
-cat("\n Complete!")
-# write results to file
-#write.csv(ar_out_a, file = paste0("attack_rates/ar_sim_data_median_",b,".csv"))
-write.csv(ar_out, file = paste0("attack_rates/ar_sim_data_mean_",b,".csv"))
-#write.csv(li_out_a, file = paste0("lifetime_infs/li_sim_data_median_",b,".csv"))
-write.csv(li_out, file = paste0("lifetime_infs/li_sim_data_mean_",b,".csv"))
-
-} # end b loop
-# } # end foreach
+} # end b bin loop
+} # b loop
 
 # stop cluster
 # stopCluster(cl)
+
+#######################################
+# read in results (rather than re-run simulations)
+setwd("~/Dropbox/Kylie/Projects/Morevac/data/sim_data/infection_histories")
+files_inf <- list.files(pattern="*.csv")
+dt_inf = do.call(rbind, lapply(files, fread))
+
+setwd("~/Dropbox/Kylie/Projects/Morevac/data/sim_data/vaccination_histories")
+files_vac <- list.files(pattern="*.csv")
+dt_vac = do.call(rbind, lapply(files_vac, fread))
+
+### summarise raw data
+# get number of vaccinations
+dt_vac1 <- dt_vac %>% mutate(Num_Vacs = rowSums(select(.,Age0:Age18)))
+dt_vac2 <- dt_vac1 %>% group_by(Vac_Strategy)
+dt_inf1 <- dt_inf %>% group_by(Vac_Strategy, Sim, Cohort, ID) %>% summarise_at(vars(starts_with('Age')), sum)
+
+
+
+
+
