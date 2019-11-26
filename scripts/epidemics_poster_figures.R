8bd8919e19d1a59b2ac7bb6fdd5b14bd28bb2cbb
Kylie Ainslie <kainslie@wpia-dide023.dide.ic.ac.uk>
2019-11-26 18:56:03 +0000

    change all histograms to densities, change color scheme

--- scripts/epidemics_poster_figures.R
+++ scripts/epidemics_poster_figures.R
@@ -6,24 +6,33 @@ setwd("~/Dropbox/Kylie/Presentations/Epidemics 2019")
 # Figure 1 - histograms of difference for off at 10 (a) & 16 (b)
 # a)
 p1a <- ggplot(data = banana_split2, aes(Mean_Diff, fill = Diff_Color)) +
-  geom_histogram(bins = 50, show.legend = FALSE) +
-  scale_fill_manual(values = c("#F8766D","#00BA38","gray85")) +
+  geom_density(alpha = 0.3) +
+  scale_fill_manual(name = 'Difference', values = c("#F8766D","#00BA38", "#619CFF"), labels = c('<0', '>0', '0')) +
   labs(x = "Difference", y = "Frequency") +
-  theme(panel.grid.major = element_blank(),
+  theme(legend.position = c(0.1, 0.9),
+        panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
-        axis.line = element_line(colour = "black"))
+        axis.line = element_line(colour = "black"),
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12))
 
 p1b <- ggplot(data = banana_split2, aes(Mean_Diff, fill = Diff_Color)) +
-  geom_histogram(bins = 50, show.legend = FALSE) +
-  scale_fill_manual(values = c("#F8766D","#00BA38","gray85")) +
+  geom_density(alpha = 0.3) +
+  scale_fill_manual(name = 'Difference', values = c("#F8766D","#00BA38", "#619CFF"), labels = c('<0', '>0', '0')) +
   labs(x = "Difference", y = "Frequency") +
-  theme(panel.grid.major = element_blank(),
+  theme(legend.position = 'none',
+        panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
-        axis.line = element_line(colour = "black"))
+        axis.line = element_line(colour = "black"),
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12))
 
-theme_set(theme_cowplot(font_size=10)) # reduce default font size
 p1 <- plot_grid(p1a, p1b, labels = "AUTO", ncol = 1, align = 'v', axis = 'l')
 
 png(file = "figure1.png", width = 6, height = 4,
@@ -36,26 +45,35 @@ dev.off()
 # b) off at 10: scatter plot of VE x Epsilon with only sig diff points
 # c) off at 16: scatter plots of all mean diff by epsilon & VE
 # d) off at 16: scatter plot of VE x Epsilon with only sig diff points
-
+banana_hammock0 <- banana_split2 %>% filter(Diff_Color == 'zero')
+banana_hammock <- banana_split2 %>% filter(Diff_Color != 'zero')
 # a)
-p2a1 <- ggplot(banana_split2, aes(x = Epsilon, y = Mean_Diff,color = Diff_Color)) +
+p2a1 <- ggplot(banana_hammock, aes(x = Epsilon, y = Mean_Diff, color = Diff_Color)) +
   geom_point() +
   geom_errorbar(aes(ymin = Lower, ymax = Upper)) +
   geom_hline(yintercept = 0, color = "black") +
-  scale_color_manual(name = 'Difference',values = c("#F8766D","#00BA38","gray85")) +
+  scale_color_manual(name = 'Difference', values = c("#F8766D","#00BA38", "#619CFF")) +
   xlab("Exposure Penalty") +
   ylab('Difference') +
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14)) +
+  geom_point(data = banana_hammock0, aes(x = Epsilon, y = Mean_Diff), alpha = 0.4) +
+  geom_errorbar(data = banana_hammock0, aes(ymin = Lower, ymax = Upper), alpha = 0.4)
+p2a1
 
-p2a2 <- ggplot(banana_split2, aes(x = VE, y = Mean_Diff,color = Diff_Color)) +
+p2a2 <- ggplot(banana_hammock, aes(x = VE, y = Mean_Diff, color = Diff_Color)) +
   geom_point() +
   geom_errorbar(aes(ymin = Lower, ymax = Upper)) +
   geom_hline(yintercept = 0, color = "black") +
-  scale_color_manual(name = 'Difference',labels = c("<0",">0","0"),
-                     values = c("#F8766D","#00BA38","gray85")) +
+  scale_color_manual(name = 'Difference', values = c("#F8766D","#00BA38", "#619CFF")) +
   xlab("Vaccine Effectiveness") +
   ylab('Difference') +
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14)) +
+  geom_point(data = banana_hammock0, aes(x = VE, y = Mean_Diff), alpha = 0.4) +
+  geom_errorbar(data = banana_hammock0, aes(ymin = Lower, ymax = Upper), alpha = 0.4)
 
 p2a <- plot_grid(p2a1, p2a2, labels = "AUTO", ncol = 1, align = 'v', axis = 'l')
 
@@ -69,27 +87,39 @@ p2b <- ggplot(data = non_zeros, aes(x = Epsilon, y = VE, color = Diff_Color)) +
   scale_color_manual(name = "Difference", values = c("#F8766D","#00BA38")) +
   xlab('Exposure Penalty') +
   ylab('VaccineEffectiveness') +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12))
 
 # c)
-p2c1 <- ggplot(banana_split2, aes(x = Epsilon, y = Mean_Diff,color = Diff_Color)) +
+# a)
+p2c1 <- ggplot(banana_hammock, aes(x = Epsilon, y = Mean_Diff, color = Diff_Color)) +
   geom_point() +
   geom_errorbar(aes(ymin = Lower, ymax = Upper)) +
   geom_hline(yintercept = 0, color = "black") +
-  scale_color_manual(name = 'Difference',values = c("#F8766D","#00BA38","gray85")) +
+  scale_color_manual(name = 'Difference', values = c("#F8766D","#00BA38", "#619CFF")) +
   xlab("Exposure Penalty") +
   ylab('Difference') +
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14)) +
+  geom_point(data = banana_hammock0, aes(x = Epsilon, y = Mean_Diff), alpha = 0.4) +
+  geom_errorbar(data = banana_hammock0, aes(ymin = Lower, ymax = Upper), alpha = 0.4)
 
-p2c2 <- ggplot(banana_split2, aes(x = VE, y = Mean_Diff,color = Diff_Color)) +
+p2c2 <- ggplot(banana_hammock, aes(x = VE, y = Mean_Diff, color = Diff_Color)) +
   geom_point() +
   geom_errorbar(aes(ymin = Lower, ymax = Upper)) +
   geom_hline(yintercept = 0, color = "black") +
-  scale_color_manual(name = 'Difference',labels = c("<0",">0","0"),
-                     values = c("#F8766D","#00BA38","gray85")) +
+  scale_color_manual(name = 'Difference', values = c("#F8766D","#00BA38", "#619CFF")) +
   xlab("Vaccine Effectiveness") +
   ylab('Difference') +
-  theme(legend.position = "none")
+  theme(legend.position = "none",
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14)) +
+  geom_point(data = banana_hammock0, aes(x = VE, y = Mean_Diff), alpha = 0.4) +
+  geom_errorbar(data = banana_hammock0, aes(ymin = Lower, ymax = Upper), alpha = 0.4)
 
 p2c <- plot_grid(p2c1, p2c2, labels = "AUTO", ncol = 1, align = 'v', axis = 'l')
 
@@ -99,13 +129,16 @@ non_zeros <- banana_split2 %>% filter(Diff_Color != "zero") %>% mutate(Abs_Val =
 
 p2d <- ggplot(data = non_zeros, aes(x = Epsilon, y = VE, color = Diff_Color)) +
   geom_point(aes(size = Abs_Val)) +
-  scale_size_continuous(range = point_range, name = "|Difference|") +
+  scale_size_continuous(name = "|Difference|") +
   scale_color_manual(name = "Difference", values = c("#F8766D","#00BA38")) +
   xlab('Exposure Penalty') +
   ylab('VaccineEffectiveness') +
-  theme(legend.position = "bottom")
+  theme(legend.position = "bottom",
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14),
+        legend.text = element_text(size=12),
+        legend.title = element_text(size=12))
 
-theme_set(theme_cowplot(font_size=10)) # reduce default font size
 p2 <- plot_grid(p2a, p2b, p2c, p2d, labels = "AUTO", ncol = 2, align = 'v', axis = 'l')
 
 png(file = "figure2.png", width = 8, height = 8,
@@ -117,23 +150,65 @@ dev.off()
 # a) off at 10: histogram of difference
 # b) off at 16: histogram of difference
 # a)
-p3a <- ggplot(data = banana_pancake2, aes(Mean_Diff, fill = Diff_Color)) +
-  geom_histogram(bins = 50, show.legend = FALSE) +
-  scale_fill_manual(values = c("#F8766D","#00BA38","gray85")) +
-  labs(x = "Difference", y = "Frequency") +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"))
+# p3a <- ggplot(data = banana_pancake2, aes(Mean_Diff, fill = Diff_Color)) +
+#   geom_histogram(bins = 50, show.legend = FALSE) +
+#   scale_fill_manual(values = c("#F8766D","#00BA38","gray85")) +
+#   labs(x = "Difference", y = "Frequency") +
+#   theme(panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         panel.background = element_blank(),
+#         axis.line = element_line(colour = "black"))
+# # b)
+# p3b <- ggplot(data = banana_pancake2, aes(Mean_Diff, fill = Diff_Color)) +
+#   geom_histogram(bins = 50, show.legend = FALSE) +
+#   scale_fill_manual(values = c("#F8766D","#00BA38","gray85")) +
+#   labs(x = "Difference", y = "Frequency") +
+#   theme(panel.grid.major = element_blank(),
+#         panel.grid.minor = element_blank(),
+#         panel.background = element_blank(),
+#         axis.line = element_line(colour = "black"))
+#
+# p3 <- plot_grid(p3a, p3b, labels = "AUTO", ncol = 1, align = 'v', axis = 'l')
+#
+# png(file = "figure3.png", width = 6, height = 4,
+#     units = "in", pointsize = 8, res = 300)
+# p3
+# dev.off()
+
+
+# Figure 3 - alternative (fully vac)
+# a) off at 10: density of difference with entire pop diff superimposed
+# b) off at 16: density of difference '' ''
+# a)
+  p3a_alt <- ggplot() +
+              geom_density(data = banana_pancake2, aes(x = Mean_Diff, fill = Diff_Color), alpha = 0.3) +
+              scale_fill_manual(name="Difference",values=c("#F8766D","#00BA38","#619CFF"), labels = c('<0', '>0', '0')) +
+              geom_density(data = banana_split2, aes(x = Mean_Diff, color = 'black'), linetype = 'dashed', fill = 'gray', alpha = 0.3) +
+              scale_color_manual(name = '', values = c('black'), labels = c('Entire Population')) +
+              xlab('Difference')+ylab('Density') +
+              theme(legend.position = c(0.8, 0.8),
+                    axis.text=element_text(size=12),
+                    axis.title=element_text(size=14),
+                    legend.text = element_text(size = 12),
+                    legend.title = element_text(size = 12))
 # b)
-p3b <- ggplot(data = banana_pancake2, aes(Mean_Diff, fill = Diff_Color)) +
-  geom_histogram(bins = 50, show.legend = FALSE) +
-  scale_fill_manual(values = c("#F8766D","#00BA38","gray85")) +
-  labs(x = "Difference", y = "Frequency") +
-  theme(panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        axis.line = element_line(colour = "black"))
+  p3b_alt <- ggplot() +
+              geom_density(data = banana_pancake2, aes(x = Mean_Diff, fill = Diff_Color), alpha = 0.3) +
+              scale_fill_manual(name="Difference",values=c("#F8766D","#00BA38","#619CFF"), labels = c('<0', '>0', '0')) +
+              geom_density(data = banana_split2, aes(x = Mean_Diff, color = 'black'), linetype = 'dashed', fill = 'gray', alpha = 0.3) +
+              scale_color_manual(name = '', values = c('black'), labels = c('Entire Population')) +
+              xlab('Difference')+ylab('Density') +
+              theme(legend.position = 'none',
+                    axis.text=element_text(size=12),
+                    axis.title=element_text(size=14)
+                    )
+
+p3_alt <- plot_grid(p3a_alt, p3b_alt, labels = "AUTO", ncol = 1, align = 'v', axis = 'l')
+
+png(file = "figure3.png", width = 6, height = 4,
+    units = "in", pointsize = 8, res = 300)
+p3_alt
+dev.off()
 
 # Table 1 of param values for |diff| > 1
 # a) off at 10: table of values where |diff| > 1
@@ -166,13 +241,13 @@ t1a <- formattable(banana_cream_pie,
 t1b <- formattable(banana_cream_pie,
                    align =c("l","c","c","c","c", "c", "c"), list(
          #`Mean_Diff` = color_bar(customRed),
-         `Vac_Cov`= color_tile(customGreen0, customGreen),
-         `Waning`= color_tile(customGreen0, customGreen),
-         `Take`= color_tile(customGreen0, customGreen),
-         `Epsilon`= color_tile(customGreen0, customGreen),
-         `Rho`= color_tile(customGreen0, customGreen),
-         `VE`= color_tile(customGreen0, customGreen)
-         ))
-
+                     `Vac_Cov`= color_tile(customGray0, customGray),
+                     `Waning`= color_tile(customGray0, customGray),
+                     `Take`= color_tile(customGray0, customGray),
+                     `Epsilon`= color_tile(customGray0, customGray),
+                     `Rho`= color_tile(customGray0, customGray),
+                     `VE`= color_tile(customGray0, customGray)
+                   ))
+t1b
 
 
