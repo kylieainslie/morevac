1074ca21dc9146b238eca63808ee467b98c74b7c
Kylie Ainslie <kainslie@wpia-dide023.dide.ic.ac.uk>
2020-01-13 13:01:11 +0000

    revert to previous code version before branching

--- R/drift_func.R
+++ R/drift_func.R
@@ -12,10 +12,10 @@
 drift_func <- function(nyears = 200, rate = 1){
   # drift for infection
   draws <- rexp(nyears, rate = rate)
-  drift <- cumsum(draws)
-  drift_data <- data.frame(x = 1:nyears, y = drift)
-  antigenic_dist <- pdist(drift_data)
+  # drift <- cumsum(draws)
+  # drift_data <- data.frame(x = 1:nyears, y = drift)
+  # antigenic_dist <- pdist(drift_data)
   # scale draws by maximum value (to put it on a scale of 0 to 1)
-  #scaled_drift <- draws/max(draws)
-  return(antigenic_dist)
+  scaled_drift <- draws/max(draws)
+  return(scaled_drfit)
 }
