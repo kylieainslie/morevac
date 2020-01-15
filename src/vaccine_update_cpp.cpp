0099ea55a0c2534ab60690fcf5b6125a77a64c05
kylieainslie <ainslie.kylie@gmail.com>
2020-01-15 17:21:06 +0000

    fix bug in vaccine_update_cpp

--- src/vaccine_update_cpp.cpp
+++ src/vaccine_update_cpp.cpp
@@ -11,16 +11,17 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 List vaccine_update_cpp(NumericMatrix drift, double threshold, double vac_protect) {
 
-  NumericVector vaccine_dist;
+  NumericVector vaccine_dist(drift.nrow());
   IntegerVector update(drift.nrow());
   int years_since_vac_update = 0;
   NumericVector gammas(drift.nrow());
 // set initial values of vaccine_dist and update vectors
-  vaccine_dist = 0;
+  vaccine_dist[0] = 0;
   update[0] = 1;
   gammas[0] = 1-vac_protect;
 // calculate vaccine distance from circulating strain
   for (int j = 1; j < drift.nrow(); ++j){
+//  Rcpp::Rcout<<years_since_vac_update<<std::endl;
     vaccine_dist[j] = drift(j, j - years_since_vac_update);
       if(vaccine_dist[j] > threshold){
         update[j] = 1;              // update vaccine
@@ -30,11 +31,12 @@ List vaccine_update_cpp(NumericMatrix drift, double threshold, double vac_protec
       } else {
         update[j] = 0;
         years_since_vac_update += 1;
-        gammas[j] = 1 - vac_protect*(1 - (1/(1 + exp(1.299*((log(300 - vaccine_dist[j]))-log(2.844))))));
+        gammas[j] = 1 - vac_protect*(1-(1/(1 + exp(1.299*(log(200) - vaccine_dist[j]) - log(2.844)))));
       }
   }
 
   List rtn;
+  rtn["distance"] = vaccine_dist;
   rtn["update"] = update;
   rtn["gammas"] = gammas;
   return(rtn);
@@ -43,5 +45,5 @@ List vaccine_update_cpp(NumericMatrix drift, double threshold, double vac_protec
 /*** R
 drift <- drift_func(nyears = 10, rate = 1)
 dist_mat <- drift$antigenic_dist
-vaccine_update_cpp(drift = dist_mat, threshold = 2.0, vac_protect = 0.7)
+vaccine_update_cpp(drift = dist_mat, threshold = 4.0, vac_protect = 0.7)
 */
