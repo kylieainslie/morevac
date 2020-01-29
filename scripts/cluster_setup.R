d04084ddc8557776035dae0bcabf9a5e7357801c
kylieainslie <ainslie.kylie@gmail.com>
2020-01-29 18:22:17 +0000

    routine updates

--- scripts/cluster_setup.R
+++ scripts/cluster_setup.R
@@ -13,7 +13,7 @@ options(didehpc.username = "kainslie",
 
 # dont really know what these lines do but you need them
 context::context_log_start()
-root <- "contexts"
+root <- "contexts2"
 
 # setting up the context: what files need to be sourced for your function to work, what packages you need
 # package sources only required here because the package I am using is not on CRAN
@@ -26,14 +26,17 @@ ctx <- context::context_save(path = root,
 # this line should give you a login prompt
 obj <- didehpc::queue_didehpc(ctx)
 
-# 'job' is the thing I am running on the cluste
+# run a single job
+# 'job' is the thing I am running on the cluster
 job <- obj$enqueue(run_sims_all(params_file = "param_values.csv", out_file = "sim"))
 job2 <- obj$enqueue(run_sims_all(params_file = "param_values_16.csv", out_file = "sim16_"))
 
 # bulk jobs
-params <- create_params_file(n_sim = 10, n_indiv = 30000, lhc_size = 10, out_file = "param_values_test",
+library(morevac)
+library(lhs)
+params <- create_params_file(n_sim = 100, n_indiv = 30000, lhc_size = 1000, out_file = "param_values",
                             vac_cutoff = 10, seed = 1234)
 
-job_bulk <- enqueue_bulk(params, run_sims_clust, do_call = TRUE)
+job_bulk2 <- obj$enqueue_bulk(params[40:50,], run_sims_clust, do_call = TRUE)
 
 
