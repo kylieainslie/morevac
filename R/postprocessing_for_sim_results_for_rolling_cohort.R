9c4fec175c43960eb35555aaf12033044956ff43
Kylie Ainslie <kainslie@dide.local>
2020-02-03 17:36:12 +0000

    change write.file to write_file

--- R/postprocessing_for_sim_results_for_rolling_cohort.R
+++ R/postprocessing_for_sim_results_for_rolling_cohort.R
@@ -5,13 +5,13 @@
 #' @param n_sim number of simulations performed (must be the same for each sim case)
 #' @param total_year_range vector of years simulations were run over (YYYY format)
 #' @param length_study number of years to get cohorts from
-#' @param write.file logical. If TRUE, writes infection and vaccination histories for cohorts to csv file
+#' @param write_file logical. If TRUE, writes infection and vaccination histories for cohorts to csv file
 #' @param file character string indicating the name of the output file. Only used when write.file = TRUE.
 #' @return data frame of attack rates within the cohort by year
 #' @keywords morevac
 #' @export
 postprocess_sim_results_for_rolling_cohort <- function(sim_dat, n_sim = 100, total_year_range = 1918:2028,
-                                                       length_study = 19, write.file = FALSE, file = "test"){
+                                                       length_study = 19, write_file = FALSE, file = "test"){
   # subset birth cohorts from each sim
   for (s in 1:n_sim){
     my_cohorts <- get_cohorts(inf_history = sim_dat$inf_history[[s]],
@@ -33,7 +33,7 @@ postprocess_sim_results_for_rolling_cohort <- function(sim_dat, n_sim = 100, tot
       }
   }
   # write file to disk
-  if (write.file){
+  if (write_file){
     try(data.table::fwrite(save_inf_hist, file = paste0(file,"_inf_hist.csv"), col.names = TRUE,
                            row.names = FALSE, sep = ","))
     try(data.table::fwrite(save_vac_hist, file = paste0(file,"_vac_hist.csv"), col.names = TRUE,
