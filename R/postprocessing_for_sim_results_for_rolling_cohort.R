d24a3bb9d6c7af044e177a3e70eb09f57b2ceea9
Kylie Ainslie <kainslie@dide.local>
2020-02-03 11:51:04 +0000

    change input to list of sparse matrices

--- R/postprocessing_for_sim_results_for_rolling_cohort.R
+++ R/postprocessing_for_sim_results_for_rolling_cohort.R
@@ -1,22 +1,22 @@
 #' Multi-annual model of infection and vaccination (version 2)
 #'
 #' This function post processes simulation results for a rolling cohort.
-#' @param sim0 array of simulations results (no vac)
-#' @param sim1 array of simulations results (annual vac)
-#' @param sim2 array of simulation results (biannual vac)
+#' @param sim0 list of sparse matrices of simulations results (no vac)
+#' @param sim1 list of sparse matrices of simulations results (annual vac)
+#' @param sim2 list of sparse matrices of simulation results (biannual vac)
 #' @param nsim number of simulations performed (must be the same for each sim case)
 #' @param total_year_range vector of years simulations were run over (YYYY format)
 #' @param cohort_year_index vector of index of years to get attack rates for
 #' @return data frame of attack rates within the cohort by year
 #' @keywords morevac
 #' @export
-postprocess_sim_results_for_rolling_cohort <- function(simdat, nsim = 100, total_year_range = 1820:2028,
+postprocess_sim_results_for_rolling_cohort <- function(sim_dat, n_sim = 100, total_year_range = 1918:2028,
                                                        length_study = 19, write.file = FALSE, file = "test"){
   # subset birth cohorts from each sim
-  for (s in 1:nsim){
-    my_cohorts <- get_cohorts(inf_history = simdat$inf_history[,,s],
-                              vac_history = simdat$vac_history[,,s],
-                              ages = simdat$ages[,,s],
+  for (s in 1:n_sim){
+    my_cohorts <- get_cohorts(inf_history = sim_dat$inf_history[[s]],
+                              vac_history = sim_dat$vac_history[[s]],
+                              ages = sim_dat$ages[[s]],
                               total_year_range = total_year_range)
 
     my_cohorts$inf_history$Sim <- my_cohorts$vac_history$Sim <- s
