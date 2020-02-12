e6350e2b5e70d908efe6abb86effd9d5bf81ddeb
Kylie Ainslie <kainslie@dide.local>
2020-02-12 12:47:39 +0000

    comment out fully vac plot

--- scripts/main_sims.R
+++ scripts/main_sims.R
@@ -138,31 +138,31 @@ data.table::fwrite(chocolate_surprise, file = "chocolate_surprise.csv", col.name
 
 ############################################
 ### Fully vaccinated individuals
-vac_max <- c(max(banana[banana$Vac_Strategy == "Annual",]$Num_Vacs), max(banana[banana$Vac_Strategy == "Biennial",]$Num_Vacs))
-banana_pancake <- banana %>%
-  filter((Vac_Strategy == 'Annual' & Num_Vacs == vac_max[1]) |
-           (Vac_Strategy == 'Biennial' & Num_Vacs == vac_max[2])) %>%
-  group_by(Vac_Strategy, Param_Index, Sim) %>%
-  summarise(Mean_Infs = mean(Num_Infs)) %>%
-  spread(Vac_Strategy, Mean_Infs) %>%
-  mutate(Difference = Annual - Biennial) %>%
-  filter(!is.na(Difference))
-
-# bootstrap
-my_bootstrap2 <- plyr::dlply(banana_pancake, "Param_Index", function(dat) boot(dat, foo, R=1000)) # boostrap for each set of param values
-my_ci2 <- sapply(my_bootstrap2, function(x) boot.ci(x, index = 1, type='perc')$percent[c(4,5)]) # get confidence intervals
-my_ci2[vapply(my_ci2, is.null, logical(1))] <- list(c(NA, NA))
-my_ci2a <- data.frame(matrix(unlist(my_ci2), nrow=length(my_ci2), byrow=T)) #bind_rows(my_ci2)
-
-banana_pancake2 <- banana_pancake %>% group_by(Param_Index) %>% summarise(Mean_Diff = mean(Difference))
-banana_pancake2$Lower <- my_ci2a[,1]
-banana_pancake2$Upper <- my_ci2a[,2]
-banana_pancake2 <- left_join(banana_pancake2, param_values, by = c("Param_Index"))
-# add Diff_Color column for plotting
-banana_pancake2$Diff_Color <- ifelse(banana_pancake2$Upper < 0, '<0',
-                                   ifelse(banana_pancake2$Lower <=0 & banana_pancake2$Upper >=0, 'zero',
-                                          ifelse(banana_pancake2$Lower >0, '>0', 'something else')))
-banana_pancake2$Diff_Color <- ifelse(is.na(banana_pancake2$Upper) & banana_pancake2$Mean_Diff <0, '<0', banana_pancake2$Diff_Color)
-banana_pancake2$Diff_Color <- ifelse(is.na(banana_pancake2$Lower) & banana_pancake2$Mean_Diff >0, '>0', banana_pancake2$Diff_Color)
+# vac_max <- c(max(banana[banana$Vac_Strategy == "Annual",]$Num_Vacs), max(banana[banana$Vac_Strategy == "Biennial",]$Num_Vacs))
+# banana_pancake <- banana %>%
+#   filter((Vac_Strategy == 'Annual' & Num_Vacs == vac_max[1]) |
+#            (Vac_Strategy == 'Biennial' & Num_Vacs == vac_max[2])) %>%
+#   group_by(Vac_Strategy, Param_Index, Sim) %>%
+#   summarise(Mean_Infs = mean(Num_Infs)) %>%
+#   spread(Vac_Strategy, Mean_Infs) %>%
+#   mutate(Difference = Annual - Biennial) %>%
+#   filter(!is.na(Difference))
+#
+# # bootstrap
+# my_bootstrap2 <- plyr::dlply(banana_pancake, "Param_Index", function(dat) boot(dat, foo, R=1000)) # boostrap for each set of param values
+# my_ci2 <- sapply(my_bootstrap2, function(x) boot.ci(x, index = 1, type='perc')$percent[c(4,5)]) # get confidence intervals
+# my_ci2[vapply(my_ci2, is.null, logical(1))] <- list(c(NA, NA))
+# my_ci2a <- data.frame(matrix(unlist(my_ci2), nrow=length(my_ci2), byrow=T)) #bind_rows(my_ci2)
+#
+# banana_pancake2 <- banana_pancake %>% group_by(Param_Index) %>% summarise(Mean_Diff = mean(Difference))
+# banana_pancake2$Lower <- my_ci2a[,1]
+# banana_pancake2$Upper <- my_ci2a[,2]
+# banana_pancake2 <- left_join(banana_pancake2, param_values, by = c("Param_Index"))
+# # add Diff_Color column for plotting
+# banana_pancake2$Diff_Color <- ifelse(banana_pancake2$Upper < 0, '<0',
+#                                    ifelse(banana_pancake2$Lower <=0 & banana_pancake2$Upper >=0, 'zero',
+#                                           ifelse(banana_pancake2$Lower >0, '>0', 'something else')))
+# banana_pancake2$Diff_Color <- ifelse(is.na(banana_pancake2$Upper) & banana_pancake2$Mean_Diff <0, '<0', banana_pancake2$Diff_Color)
+# banana_pancake2$Diff_Color <- ifelse(is.na(banana_pancake2$Lower) & banana_pancake2$Mean_Diff >0, '>0', banana_pancake2$Diff_Color)
 # randomly select fully vac individuals and plot exposure history
 
