424b5e459e123ce71a264077b4e715d89dc7fc0e
Kylie Ainslie <ainslie.kylie@gmail.com>
2020-08-20 14:31:18 +0100

    remake drift plot and fill in white space

--- scripts/methods_figures.R
+++ scripts/methods_figures.R
@@ -5,47 +5,110 @@
 max_age = 80
 vac_cut_off <- 10
 my_years <- 1918:2028
-vac_cov_dat <- data.frame(Age = 0:(max_age-1), No_Vac = numeric(max_age), Annual = numeric(max_age), Biennial = numeric(max_age))
+vac_cov_dat <- data.frame(
+  Age = 0:(max_age-1),
+  No_Vac = numeric(max_age),
+  Annual = numeric(max_age),
+  Biennial = numeric(max_age)
+  )
+
 vac_cov_dat$Annual[3:(vac_cut_off + 1)] <- 0.44
 vac_cov_dat$Biennial[seq(3,vac_cut_off+1,2)] <- 0.44
 
-out <- multiannual(n=30000, years = my_years, max_age = max_age, betas = c(0.4,rep(0.2,length(my_years)-1)),
-                   vac_coverage = vac_cov_dat$Annual, vac_strategy = 1)
+out <- multiannual(n = 30000,
+                   years = my_years,
+                   max_age = max_age,
+                   betas = c(0.4,rep(0.2,length(my_years)-1)),
+                   vac_coverage = vac_cov_dat$Annual,
+                   vac_strategy = 1,
+                   seed = 1234
+                   )
 
-#my_cohorts <- get_cohorts(inf_history = out$inf_history$inf_hist_mat, vac_history = out$vac_history$vac_hist_mat, ages = out$ages, total_year_range = my_years)
 # pick random subset of people susceptibility
+colnames(out$ages) <- paste0("year_", my_years)
+
 birth_cohort <- which(out$ages[,"year_2020"] == 0)
-indivs <- sample(birth_cohort, 9, replace = FALSE)
-person <- data.frame(Year = 2000:2019,
-                     inf_hist = indivs$inf_hist[i,],
-                     vac_hist = indivs$vac_hist[i,],
-                     suscept  = bc_suscept[i,])
-vax <- person$Year[which(person$vac_hist == 1)]
-infections <- person$Year[which(person$inf_hist == 1)]
-person
+indivs <- data.frame(id = sample(birth_cohort, 9, replace = FALSE))
+year_index <- which(my_years %in% 2000:2020)
+
+sample_profiles <- list(
+  inf_hist = out$inf_history$inf_hist_mat[indivs$id, year_index],
+  vac_hist = out$vac_history$vac_hist_mat[indivs$id, year_index],
+  suscept_mat = out$inf_history$suscept_mat[indivs$id, year_index]
+)
+
+for (i in seq_along(sample_profiles)){
+  colnames(sample_profiles[[i]]) <- paste0("year_",2000:2020)
+}
+
+person <- bind_rows(data.frame(id = indivs, sample_profiles$inf_hist),
+                    data.frame(id = indivs, sample_profiles$vac_hist),
+                    data.frame(id = indivs, sample_profiles$suscept_mat),
+                    .id = "profile") %>%
+  mutate(profile = case_when(
+    profile == 1 ~ "inf_hist",
+    profile == 2 ~ "vac_hist",
+    profile == 3 ~ "suscept_mat"
+  )) %>%
+  pivot_longer(cols = starts_with("year_"),
+               names_to = "year",
+               names_prefix = "year_",
+               values_to = "value")
+
+# do I need this code?
+vax <- person %>%
+  filter(profile == "vac_hist",
+         value == 1)
+
+infections <- person %>%
+  filter(profile == "inf_hist",
+         value == 1)
+###########
+
+### Top panel ###
 # drift plot
-drift_dat <- data.frame(Year = 2000:2019, Drift = out$drift$y[181:200], Vac_Update = out$vac_update[181:200],
-                        Gammas = out$gammas[181:200])
+drift_dat <- data.frame(Year = my_years[year_index],
+                        Drift = out$drift$drift$y[year_index],
+                        Vac_Update = out$vac_update[year_index],
+                        Gammas = out$gammas[year_index]
+                        )
 drift_dat$Shade_Group <- c(rep(1,dim(drift_dat)[1]))
+
 for (i in 2:dim(drift_dat)[1]){
   if (drift_dat$Vac_Update[i]==1){drift_dat$Shade_Group[i] <- drift_dat$Shade_Group[i-1] + 1
   } else {drift_dat$Shade_Group[i] <- drift_dat$Shade_Group[i-1]}
 }
+
 drift_dat$Shade_Group <- (drift_dat$Shade_Group)
+
 #calculate cumulative drift
 drift_dat$Cum_Drift <- cumsum(drift_dat$Drift)
-drift_dat$Year  <-(drift_dat$Year)
+drift_dat$Year  <- (drift_dat$Year)
+
 # determine years when a vaccine update occurred
 drift_dat_thinned <- drift_dat[which(drift_dat$Vac_Update==1),]
 drift_dat_thinned$prev <-c(min(drift_dat$Year),drift_dat_thinned$Year[-nrow(drift_dat_thinned)])
+drift_dat_thinned <- drift_dat_thinned %>%
+  add_row(Year = 2020,
+          Drift = drift_dat[drift_dat$Year == 2020,]$Drift,
+          Vac_Update = drift_dat[drift_dat$Year == 2020,]$Vac_Update,
+          Gammas = drift_dat[drift_dat$Year == 2020,]$Gammas,
+          Shade_Group = drift_dat[drift_dat$Year == 2020,]$Shade_Group + 1,
+          Cum_Drift = drift_dat[drift_dat$Year == 2020,]$Cum_Drift,
+          prev = 2018)
+
 # plot
-ymx <- max(drift_dat$Cum_Drift)
-p_drift <- ggplot(data = drift_dat, mapping=aes( x=Year,y = Cum_Drift))+
-  scale_x_continuous(limits = c(2000,2019), expand = c(0,0)) +
-  scale_y_continuous(limits = c(0,ymx), expand = c(0,0)) +
-  geom_rect(data=drift_dat_thinned,aes(xmin=prev,ymin=0,ymax=ymx,xmax=Year,fill=factor(Shade_Group),col=factor(Shade_Group)),alpha=0.2) +
-  geom_ribbon(aes(ymin=Cum_Drift,ymax=ymx),fill="white" ,alpha=1)+  # color shading under cumulative drift by shade group
-  ylab('Cumulative Antigenic Drift') +
+ymn <- min(drift_dat$Drift)
+ymx <- max(drift_dat$Drift)
+
+p_drift <- ggplot(data = drift_dat, mapping=aes( x=Year, y = Drift))+
+  scale_x_continuous(limits = c(2000,2021), expand = c(0,0)) +
+  scale_y_continuous(limits = c(ymn,ymx), expand = c(0,0)) +
+  geom_rect(data = drift_dat_thinned,
+            aes(xmin = prev, ymin = ymn, ymax = ymx, xmax = Year, fill = factor(Shade_Group),
+                col = factor(Shade_Group)), alpha = 0.2) +
+  geom_ribbon(aes(ymin = Drift,ymax = ymx), fill = "white", alpha = 1)+  # color shading under cumulative drift by shade group
+  ylab('Antigenic Drift') +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
@@ -53,21 +116,19 @@ p_drift <- ggplot(data = drift_dat, mapping=aes( x=Year,y = Cum_Drift))+
         legend.position = "none") +
   geom_line()
 p_drift
+
 # determine colors used
 cols <- ggplot_build(p_drift)$data[[1]]$colour
-# add points in same colors
-p_drift <- p_drift + geom_point(data = drift_dat_thinned,aes(x=Year,y=Cum_Drift),colour = c(cols[-1],"#CC6666"))
-p_drift
-# vaccine protection plot
-p_gamma <- ggplot(data = drift_dat, aes(x = Year, y = Gammas)) +
-  geom_line() +
-  geom_ribbon(aes(ymax = Cum_Drift,fill = Shade_Group),ymin=0,alpha=0.2,) +
-  geom_point(data=drift_dat_thinned,aes(x=Year,y=Cum_Drift),colour = 'blue') +
-  ylab('Cumulative Antigenic Drift')
-p_gamma
 
+# add points in same colors
+p_drift <- p_drift + geom_point(data = drift_dat_thinned,aes(x=Year,y = Drift), colour = c(cols[-1],"#CC6666"))
+p_drift
+#################
+
+### Bottom panel ###
 # susceptibility plot
-p_suscept <- ggplot(data = person, aes(x = Year, y = suscept)) +
+
+p_suscept <- ggplot(data = person %>% filter(profile == "suscept_mat"), aes(x = year, y = suscept)) +
   geom_line() +
   geom_ribbon(aes(ymax = suscept),ymin=0,alpha=0.1,) +
   xlab('Year') +
@@ -83,13 +144,31 @@ p_suscept <- p_suscept +
   geom_vline(xintercept=vax, linetype="dashed", colour = vac_cols) +
   geom_vline(xintercept=infections, colour = 'red')
 p_suscept
+#################
+
+
+### Put the two panels together
 library(cowplot)
 theme_set(theme_cowplot(font_size=12)) # reduce default font size
 figure1 <- plot_grid(p_drift, p_suscept, labels = "AUTO", ncol = 1, align = 'v', axis = 'l')
 
+### Output
 # save figure
 filename <- "~/Dropbox/Kylie/Projects/Morevac/figures/"
 png(file = paste0(filename,"figure1.png"), width = 6, height = 8,
     units = "in", pointsize = 8, res = 300)
 plot(figure1)
 dev.off()
+
+
+
+### old code
+
+# vaccine protection plot
+# p_gamma <- ggplot(data = drift_dat, aes(x = Year, y = Gammas)) +
+#   geom_line() +
+#   geom_ribbon(aes(ymax = Cum_Drift,fill = Shade_Group),ymin=0,alpha=0.2,) +
+#   geom_point(data=drift_dat_thinned,aes(x=Year,y=Cum_Drift),colour = 'blue') +
+#   ylab('Cumulative Antigenic Drift')
+# p_gamma
+
