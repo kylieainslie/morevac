85ebc735c230a5081db3a8deb983ddf43faa93e2
Kylie Ainslie <ainslie.kylie@gmail.com>
2020-10-02 10:25:08 +0100

    add documentation arguments (get_cohorts.R)

--- R/get_cohorts.R
+++ R/get_cohorts.R
@@ -4,16 +4,18 @@
 #' @param inf_history sparse matrix of infection histories for each person
 #' @param vac_history sparse matrix of vaccination history for each person
 #' @param ages matrix of ages of each person for every year
-#' @param years vector of years to run simulation over (YYYY format)
-#' @param year_index vector of index of years to get attack rates for
+#' @param enrollment_start_year year for cohort enrollment to start (YYYY format)
+#' @param n_cohorts number of cohorts to enroll
+#' @param total_year_range vector of years to run simulations over
+#' @param length_study number of years to follow each cohort
 #' @param write.file logical. if true, the infection histories and vaccination
 #' histories are written to the working directory
 #' @param file character string used as the prefix for the output file if write.file = TRUE
 #' @return data frame of attack rates within the cohort by year
 #' @keywords morevac
 #' @export
-get_cohorts <- function(inf_history, vac_history, ages, enrollment_start_year = 2000, ncohorts = 10, total_year_range,
-                        length_study = 18, write.file = FALSE, file = "test"){
+get_cohorts <- function(inf_history, vac_history, ages, enrollment_start_year = 2000, ncohorts = 10,
+                        total_year_range, length_study = 18, write.file = FALSE, file = "test"){
   enrollment_stop_year = enrollment_start_year + ncohorts
   year_index <- which(total_year_range %in% enrollment_start_year:enrollment_stop_year)
   cohort = list(); cohort_inf_hist = list(); cohort_vac_hist = list(); cohort_ages = list()
