77eb38d5936d690bae135be425dd8cd7a972f7ec
Kylie Ainslie <kainslie@dide.local>
2020-10-25 08:53:53 +0000

    remove bootstrapping code

--- inst/extdata/scripts/main_sims.R
+++ inst/extdata/scripts/main_sims.R
@@ -35,7 +35,8 @@ setwd("C:/Users/kainslie/Dropbox/Kylie/Projects/Morevac/data/sim_data/")
  ### Baseline
 #######################################
 # run simulations
-run_sims_all(params_file = "param_values_baseline_time-varying_beta.csv", index = c(1,2,4,6,9,11), out_file = "sim_baseline2_")
+run_sims_all(params_file = "param_values_test.csv", out_file = "sim_test_")
+
 #######################################
  ### Latin hypercube
 #######################################
@@ -49,146 +50,20 @@ run_sims_all(params_file = "param_values_10.csv", index = 121:140, out_file = "s
 #######################################
  ### Simulation results post-processing
 #######################################
-# read in parameter file
-param_path <- "C:/Users/kainslie/Dropbox/Kylie/Projects/Morevac/data/sim_data/"
-
-param_values <- read.csv(paste0(param_path,"param_values_baseline_time-varying_beta.csv"), header = TRUE)
-names(param_values)[1] <- "Param_Index"
-
 # set working directory where results files are located
 # setwd("~/Dropbox/Kylie/Projects/Morevac/data/sim_data/cutoff10")
-setwd("C:/Users/kainslie/Dropbox/Kylie/Projects/Morevac/data/sim_data/baseline/sim500/time-varying_beta")
+setwd("C:/Users/kainslie/Dropbox/Kylie/Projects/Morevac/data/sim_data/baseline/sim1000")
 
 # read in results (rather than re-run simulations)
+files_mean_infs <- list.files(pattern="mean_infs*")
+mean_infs <- vroom(files_mean_infs)
 
-files_inf <- data.frame(file_name = list.files(pattern="*inf_hist.csv"),
-                        file_name_for_sep = list.files(pattern="*inf_hist.csv")) %>%
-  separate(col = file_name_for_sep, into = c("x", "cut_off", "param_index", "file_type", "y"), sep = "_") %>%
-  mutate(param_index = as.numeric(param_index)) %>%
-  arrange(param_index)
+files_mean_diff <- list.files(pattern="mean_diff*")
+mean_diff <- vroom(files_mean_diff)
 
-files_vac <- data.frame(file_name = list.files(pattern="*vac_hist.csv"),
-                        file_name_for_sep = list.files(pattern="*vac_hist.csv")) %>%
-  separate(col = file_name_for_sep, into = c("x", "cut_off", "param_index", "file_type", "y"), sep = "_") %>%
-  select(-x,-y) %>%
-  mutate(param_index = as.numeric(param_index)) %>%
-  arrange(param_index)
+files_mean_ar <- list.files(pattern="mean_ar*")
+mean_ar <- vroom(files_mean_ar)
 
-all_files <- left_join(files_vac, files_inf, by = "param_index")
-n_files <- dim(all_files)[1]
-
-### create progress bar
-start_index <- 1
-for (i in start_index:n_files){
-### read in data from list of files
-  print(all_files$file_name.y[i])
-  dt_inf = vroom(file = all_files$file_name.y[i], delim = ",", col_names = TRUE)  %>%
-              rename("Vac_Strategy" = "Vac_Strategy...1",
-                     "Vac_Strategy_num" = "Vac_Strategy...3") %>%
-              mutate(Num_Infs = rowSums(select(.,Age0:Age18)), Param_Index = files_inf$param_index[i])
-
-  print(all_files$file_name.x[i])
-  dt_vac = vroom(file = all_files$file_name.x[i], delim = ",", col_names = TRUE) %>%
-              rename("Vac_Strategy" = "Vac_Strategy...1",
-                     "Vac_Strategy_num" = "Vac_Strategy...3") %>%
-              mutate(Num_Vacs = rowSums(select(.,Age0:Age18)), Param_Index = files_vac$param_index[i])
-  if(length(unique(dt_vac$Vac_Strategy))<3){next} # make sure vac data contains all three vac strategies
-
-#######################################
-# summarise raw data for childhood infs
-  banana <- bind_cols(dt_inf[,c("Vac_Strategy", "Sim", "Cohort","Param_Index", "ID", "Num_Infs")], Num_Vacs = dt_vac[,c("Num_Vacs")]) %>%
-                group_by(Vac_Strategy, Sim) %>% summarise(Mean_Infs = mean(Num_Infs))
-
-#######################################
-### Bootstrapping
-#######################################
-
-# bootstrap to get CI for childhood infs
-  foo1 <- function(data, indices){
-    dt<-data[indices,]
-    mean(dt$Mean_Infs)
-  }
-  my_bootstrap <- plyr::dlply(banana, "Vac_Strategy", function(dat) boot(dat, foo1, R=100)) # boostrap for each set of param values
-  my_ci <- sapply(my_bootstrap, function(x) boot.ci(x, index = 1, type='perc')$percent[c(4,5)]) # get confidence intervals
-  banana_boat <- banana %>% group_by(Vac_Strategy) %>% summarise(Mean_Infs = mean(Mean_Infs)) %>%
-                  mutate(Lower = my_ci[1,], Upper = my_ci[2,], Param_Index = all_files$param_index[i])
-
-# Difference in childhood infs
-  banana_split <- banana %>% spread(Vac_Strategy, Mean_Infs) %>%
-                    mutate(Diff_AB = Annual - Biennial, Diff_AN = Annual - No_Vac, Diff_BN = Biennial - No_Vac) %>%
-                    select(Sim, Diff_AB, Diff_AN, Diff_BN) %>%
-                    gather(Type, Difference, Diff_AB:Diff_BN)
-
-# bootstrap to get CI for Difference
-  foo2 <- function(data, indices){
-    dt<-data[indices,]
-    mean(dt$Difference)
-  }
-  my_bootstrap <- plyr::dlply(banana_split, "Type", function(dat) boot(dat, foo2, R=100)) # boostrap for each set of param values
-  my_ci <- sapply(my_bootstrap, function(x) boot.ci(x, index = 1, type='perc')$percent[c(4,5)]) # get confidence intervals
-
-  banana_split2 <- banana_split %>% group_by(Type) %>% summarise(Mean_Diff = mean(Difference)) %>%
-                      mutate(Lower = my_ci[1,], Upper = my_ci[2,], Param_Index = all_files$param_index[i])
-
-# summarise raw data for attack rates
-  chocolate_sprinkles <- dt_inf %>% group_by(Vac_Strategy, Sim, Cohort, Param_Index) %>% do(tail(.,1))
-  chocolate_bar <- dt_inf %>% group_by(Vac_Strategy, Sim, Cohort, Param_Index) %>% select(-ID) %>% summarise_all(list(sum))
-  chocolate_bar$ID <- chocolate_sprinkles$ID
-  chocolate_sundae <- chocolate_bar %>% mutate_at(vars(Age0:Age18), funs(./ID)) %>%
-                        select(Param_Index, Vac_Strategy, Sim, Cohort, Age0:Age18) %>% group_by(Param_Index, Vac_Strategy, Sim) %>%
-                        summarise_at(vars(Age0:Age18), mean) %>% gather(Age, Attack_Rate, Age0:Age18) %>%
-                        mutate(Age = as.numeric(str_remove(Age, 'Age')))
-
-# bootstrap to get CI for ARs
-  foo3 <- function(data, indices){
-    dt<-data[indices,]
-    mean(dt$Attack_Rate)
-  }
-  my_bootstrap <- plyr::dlply(chocolate_sundae, c("Param_Index","Vac_Strategy","Age"), function(dat) boot(dat, foo3, R=1000)) # boostrap for each set of param values
-  my_ci <- sapply(my_bootstrap, function(x) boot.ci(x, index = 1, type='perc')$percent[c(4,5)]) # get confidence intervals
-
-  chocolate_sundae2 <- chocolate_sundae %>% group_by(Param_Index, Vac_Strategy, Age) %>% summarise(Mean_AR = mean(Attack_Rate)) %>%
-                          ungroup() %>% mutate(Lower = my_ci[1,], Upper = my_ci[2,])
-
-  if(i > start_index) {
-    banana_cream_pie <- bind_rows(banana_cream_pie,banana_split2)
-    banana_bread <- bind_rows(banana_bread,banana_boat)
-    chocolate_surprise <- bind_rows(chocolate_surprise, chocolate_sundae2)
-  } else {banana_cream_pie <- banana_split2
-          banana_bread <- banana_boat
-          chocolate_surprise <- chocolate_sundae2
-  }
-
-  # remove dt_inf and dt_vac from memory
-  rm(dt_inf)
-  rm(dt_vac)
-
-  # write out files in case for loop stops
-  data.table::fwrite(banana_cream_pie, file = paste0("banana_cream_pie_tmp",start_index,".csv"), col.names = TRUE,
-                     row.names = FALSE, sep = ",")
-  data.table::fwrite(banana_bread, file = paste0("banana_bread_tmp",start_index,".csv"), col.names = TRUE,
-                     row.names = FALSE, sep = ",")
-  data.table::fwrite(chocolate_surprise, file = paste0("chocolate_surprise_tmp",start_index,".csv"), col.names = TRUE,
-                     row.names = FALSE, sep = ",")
-
-
-  }
-
-#######################################
-### Create summary output files
-
-# bind columns with parameter values
-banana_cream_pie <- left_join(banana_cream_pie, param_values, by = c("Param_Index"))
-banana_bread <- left_join(banana_bread, param_values, by = c("Param_Index"))
-chocolate_surprise <- left_join(chocolate_surprise, param_values, by = c("Param_Index"))
-
-# write files to avoid having to read in raw data again
-data.table::fwrite(banana_cream_pie, file = "banana_cream_pie_baseline2.csv", col.names = TRUE,
-                   row.names = FALSE, sep = ",")
-data.table::fwrite(banana_bread, file = "banana_bread_baseline2.csv", col.names = TRUE,
-                   row.names = FALSE, sep = ",")
-data.table::fwrite(chocolate_surprise, file = "chocolate_surprise_baseline2.csv", col.names = TRUE,
-                   row.names = FALSE, sep = ",")
 
 ############################################
 ### Fully vaccinated individuals
