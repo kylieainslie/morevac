113710260debf6d8ac2a554425bf048da363ee59
kylieainslie <ainslie.kylie@gmail.com>
2022-02-17 06:45:17 +0100

    change methods plot

--- inst/extdata/scripts/methods_figures.R
+++ inst/extdata/scripts/methods_figures.R
@@ -15,8 +15,9 @@ library(data.table)
 library(rdist)
 library(Matrix)
 library(vroom)
-devtools::load_all()
-
+#devtools::load_all()
+devtools::install_github("kylieainslie/morevac")
+library(morevac)
 # run multi-annual model
 max_age = 80
 vac_cut_off <- 10
@@ -49,9 +50,9 @@ indivs <- data.frame(id = sample(birth_cohort, 9, replace = FALSE))
 year_index <- which(my_years %in% 2000:2020)
 
 sample_profiles <- list(
-  inf_hist = out$inf_history$inf_hist_mat[indivs$id, year_index],
-  vac_hist = out$vac_history$vac_hist_mat[indivs$id, year_index],
-  suscept_mat = out$inf_history$suscept_mat[indivs$id, year_index]
+  inf_hist = out$inf_history[indivs$id, year_index],
+  vac_hist = out$vac_history[indivs$id, year_index],
+  suscept_mat = out$suscept_mat[indivs$id, year_index]
 )
 
 for (i in seq_along(sample_profiles)){
@@ -79,9 +80,27 @@ person <- bind_rows(data.frame(id = indivs, sample_profiles$inf_hist),
 # drift plot
 drift_dat <- data.frame(Year = my_years[year_index],
                         Drift = out$drift$drift$y[year_index],
+                        Distance = out$drift$antigenic_dist[1, year_index],
                         Vac_Update = out$vac_update[year_index],
-                        Gammas = out$gammas[year_index]
-                        )
+                        Distance_Update = out$distance[year_index]
+                        #Gammas = out$gammas[year_index]
+                        ) %>%
+  select(Year, Vac_Update, Distance_Update)
+# find update rows
+update_rows <- which(drift_dat$Vac_Update == 1)
+
+# add rows where Distance Update = 0 (for plot)
+for (i in 1:length(update_rows)){
+  print(update_rows[i])
+  drift_dat <- drift_dat %>%
+    add_row(Year = drift_dat[update_rows[i],]$Year,
+            Vac_Update = 1,
+            Distance_Update = 0,
+            .after = update_rows[i])
+  update_rows <- update_rows + 1
+}
+
+# colors for plot
 drift_dat$Shade_Group <- c(rep(1,dim(drift_dat)[1]))
 
 for (i in 2:dim(drift_dat)[1]){
@@ -98,40 +117,45 @@ drift_dat$Year  <- (drift_dat$Year)
 # determine years when a vaccine update occurred
 drift_dat_thinned <- drift_dat[which(drift_dat$Vac_Update==1),]
 drift_dat_thinned$prev <-c(min(drift_dat$Year),drift_dat_thinned$Year[-nrow(drift_dat_thinned)])
+drift_dat_thinned$ymn <-c(min(drift_dat$Distance_Update),drift_dat_thinned$Distance_Update[-nrow(drift_dat_thinned)])
 drift_dat_thinned <- drift_dat_thinned %>%
   add_row(Year = 2020,
-          Drift = drift_dat[drift_dat$Year == 2020,]$Drift,
+          #Drift = drift_dat[drift_dat$Year == 2020,]$Drift,
           Vac_Update = drift_dat[drift_dat$Year == 2020,]$Vac_Update,
-          Gammas = drift_dat[drift_dat$Year == 2020,]$Gammas,
+          Distance_Update = drift_dat[drift_dat$Year == 2020,]$Distance_Update,
           Shade_Group = drift_dat[drift_dat$Year == 2020,]$Shade_Group + 1,
-          Cum_Drift = drift_dat[drift_dat$Year == 2020,]$Cum_Drift,
-          prev = 2018)
+          #Cum_Drift = drift_dat[drift_dat$Year == 2020,]$Cum_Drift,
+          prev = 2018,
+          ymn = drift_dat[drift_dat$Year == 2018,]$Distance_Update)
 
 # plot
-ymn <- min(drift_dat$Drift)
-ymx <- max(drift_dat$Drift)
+ymn <- min(drift_dat$Distance_Update)
+ymx <- max(drift_dat$Distance_Update)
 
-p_drift <- ggplot(data = drift_dat, mapping=aes( x=Year, y = Drift))+
+p_drift <- ggplot(data = drift_dat, mapping=aes( x=Year, y = Distance_Update))+
   scale_x_continuous(limits = c(2000,2021), expand = c(0,0)) +
   scale_y_continuous(limits = c(ymn,ymx), expand = c(0,0)) +
   geom_rect(data = drift_dat_thinned,
-            aes(xmin = prev, ymin = ymn, ymax = ymx, xmax = Year, fill = factor(Shade_Group),
-                col = factor(Shade_Group)), alpha = 0.2) +
-  geom_ribbon(aes(ymin = Drift,ymax = ymx), fill = "white", alpha = 1)+  # color shading under cumulative drift by shade group
-  ylab('Antigenic Drift') +
+            aes(xmin = prev, xmax = Year,
+                ymin = ymn, ymax = Distance_Update,
+                fill = factor(Shade_Group), col = factor(Shade_Group)), alpha = 0.2) +
+  # geom_ribbon(aes(xmin = Year, xmax = lead(Year),
+  #                 ymin = ymn, ymax = Distance_Update),
+  #             fill = "white", alpha = 1)+  # color shading under cumulative drift by shade group
+  ylab('Antigenic Distance') +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
         axis.line = element_line(colour = "black"),
         legend.position = "none") +
-  geom_line()
+  geom_step()
 p_drift
 
 # determine colors used
 cols <- ggplot_build(p_drift)$data[[1]]$colour
 
 # add points in same colors
-p_drift <- p_drift + geom_point(data = drift_dat_thinned,aes(x=Year,y = Drift), colour = c(cols[-1],"#CC6666"))
+p_drift <- p_drift + geom_point(data = drift_dat_thinned,aes(x=Year,y = Distance_Update), colour = c(cols[-1],"#CC6666"))
 p_drift
 #################
 
@@ -139,7 +163,7 @@ p_drift
 # susceptibility plot
 
 # get suscept mat
-my_id <- unique(person$id)[1]
+my_id <- sample(unique(person$id),1)
 
 suscept_mat <- person %>%
   filter(profile == "suscept_mat",
@@ -160,11 +184,12 @@ infections <- person %>%
 # plot
 
 p_suscept <- ggplot(data = suscept_mat, aes(x = year, y = value)) +
-  geom_line() +
-  geom_ribbon(aes(ymax = value),ymin=0,alpha=0.1,) +
+  geom_step() +
+  geom_rect(aes(xmin = year, xmax = lead(year),
+                ymin = 0,ymax = value),alpha=0.1,) +
   xlab('Year') +
   ylab('Susceptibility') +
-  scale_x_continuous(limits = c(2000,2020), expand = c(0,0)) +
+  scale_x_continuous(limits = c(1999,2021), expand = c(0,0)) +
   scale_y_continuous(limits = c(0,1), expand = c(0,0)) +
   theme(panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
